becf662afe0ed5b9230d531486ac6bb13bc576ca
5schatten <f.hartung@outlook.de>
2018-06-12 20:59:30 +0200

    binutils: build and install ar for target / linux: build zlib before binutils / busybox: disable ar

--- packages/devel/binutils/package.mk
+++ packages/devel/binutils/package.mk
@@ -68,10 +68,13 @@ make_target() {
   make configure-host
   make -C libiberty
   make -C bfd
+  make -C binutils ar
 }
 
 makeinstall_target() {
   mkdir -p $SYSROOT_PREFIX/usr/lib
     cp libiberty/libiberty.a $SYSROOT_PREFIX/usr/lib
   make DESTDIR="$SYSROOT_PREFIX" -C bfd install
+  mkdir -p $INSTALL/usr/bin
+  cp binutils/ar $INSTALL/usr/bin/ar
 }
