1de2cbad5b3b14687d26752a90c75ac6a35b507b
5schatten <f.hartung@outlook.de>
2018-06-12 21:14:44 +0200

    enet: updated to git 39a72ab & changed git project url

--- packages/addons/addon-depends/enet/package.mk
+++ packages/addons/addon-depends/enet/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="enet"
-PKG_VERSION="e33ca1d"
-PKG_SHA256="0ba5547de2c4c7fc79d367179a9bc92a7ac27e9258dd50fb277cd8761afaf9b0"
+PKG_VERSION="39a72ab"
+PKG_SHA256="fc9b0eaf3f4d5a20ad138d8414a9586ca9ab5e8f44b33d9de4eacb40bbe07f0d"
 PKG_ARCH="any"
 PKG_LICENSE=""
-PKG_SITE="https://github.com/cgutman/enet/"
-PKG_URL="https://github.com/cgutman/enet/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/lsalzman/enet"
+PKG_URL="https://github.com/lsalzman/enet/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="network"
 PKG_SHORTDESC="ENet's purpose is to provide a relatively thin, simple and robust network communication layer on top of UDP (User Datagram Protocol)"
