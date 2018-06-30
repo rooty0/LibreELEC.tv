bb6350888c4de6f59a05265aa9aeb00d0dabee0e
5schatten <f.hartung@outlook.de>
2018-06-30 15:46:08 +0200

    libjpeg-turbo: build shared lib

--- packages/graphics/libjpeg-turbo/package.mk
+++ packages/graphics/libjpeg-turbo/package.mk
@@ -21,7 +21,7 @@ PKG_CONFIGURE_OPTS_HOST="--enable-static \
                          --with-jpeg8 \
                          --without-simd"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared --with-jpeg8"
+PKG_CONFIGURE_OPTS_TARGET="--with-jpeg8"
 
 if ! target_has_feature "(neon|sse)"; then
   PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_TARGET --without-simd"
