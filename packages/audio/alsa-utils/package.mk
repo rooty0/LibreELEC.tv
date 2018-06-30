35a7b1ac290613d87a299d46a40c81d1ff02f316
5schatten <f.hartung@outlook.de>
2018-06-30 15:54:37 +0200

    alsa-utils: add asound.conf.sample if not a RPi project

--- packages/audio/alsa-utils/package.mk
+++ packages/audio/alsa-utils/package.mk
@@ -28,6 +28,12 @@ post_makeinstall_target() {
   rm -rf $INSTALL/usr/share/sounds
   rm -rf $INSTALL/usr/lib/systemd/system
 
+# install sample asound.conf
+  if [ $PROJECT != "RPi" ]; then
+    mkdir -p $INSTALL/usr/config
+    cp $PKG_DIR/config/* $INSTALL/usr/config/
+  fi
+
 # remove default udev rule to restore mixer configs, we install our own.
 # so we avoid resetting our soundconfig
   rm -rf $INSTALL/usr/lib/udev/rules.d/90-alsa-restore.rules
