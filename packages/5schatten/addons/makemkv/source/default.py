c1b02bdc7a66fb455a094205bf13f0cb6d779493
5schatten <f.hartung@outlook.de>
2018-07-29 14:01:34 +0200

    makemkv: initial package v1.12.3

---
+++ packages/5schatten/addons/makemkv/source/default.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018 Made by Escalade (https://github.com/escalade)
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+import xbmcgui
+
+dialog = xbmcgui.Dialog()
+dialog.ok('', 'This is a console-only addon')
