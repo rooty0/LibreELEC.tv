4691accb473cfd5dee1b31543493064056075687
5schatten <f.hartung@outlook.de>
2018-07-29 14:44:44 +0200

    oem: added apps, emulators & tools to oem pkg / added start & function scripts / added not joystick rules

---
+++ packages/oem/files/usr/bin/kodifreeze.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+# Script to suspend/resume audio and freeze/unfreeze the Kodi process
+
+kodi_freeze() {
+  kodi-send --action="RunScript(/usr/bin/audio-suspend.py)"
+  usleep 500000
+  if [ ! "$1" = "muteonly" ]; then
+    systemctl stop kodi &
+  fi
+}
+
+kodi_unfreeze() {
+  if [ "$1" = "muteonly" ]; then
+    kodi-send --action="RunScript(/usr/bin/audio-resume.py)"
+  else
+    systemctl start kodi
+  fi
+}
+
+case $1 in
+  freeze)
+    if [ ! -f /tmp/.kodifreeze ]; then
+      pidof kodi.bin > /dev/null && KODI=1
+      if [ "$KODI" = "1" ]; then
+        kodi_freeze $2
+        touch /tmp/.kodifreeze
+      else
+        echo "Kodi isn't running, nothing to do."
+      fi
+    else
+      echo "Kodi already frozen, nothing to do."
+    fi
+    ;;
+  unfreeze)
+    if [ -f /tmp/.kodifreeze ]; then
+      kodi_unfreeze $2
+      rm /tmp/.kodifreeze
+    else
+      echo "Kodi isn't frozen, nothing to do."
+    fi
+    ;;
+  *)
+    echo "Usage: $0 [freeze|unfreeze] [muteonly]"
+    ;;
+esac
--- packages/oem/package.mk
+++ packages/oem/package.mk
@@ -1,13 +1,101 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="oem"
-PKG_VERSION=""
+PKG_VERSION="1.0"
 PKG_ARCH="any"
 PKG_LICENSE="various"
 PKG_SITE="http://www.libreelec.tv"
-PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain $PROJECT"
-PKG_SECTION="virtual"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="oem"
 PKG_SHORTDESC="OEM: Metapackage for various OEM packages"
 PKG_LONGDESC="OEM: Metapackage for various OEM packages"
+
+PKG_TOOLCHAIN="manual"
+
+OEM_APPS_COMMON=" \
+  ds4drv \
+  htop \
+  mc \
+  sdl-jstest"
+
+OEM_APPS_GENERIC=" \
+  dmidecode \
+  google-chrome \
+  smartmontools \
+  spotify"
+
+OEM_APPS_RPI=" \
+  librespot"
+
+OEM_EMU_COMMON=" \
+  emulationstation-retropie \
+  retroarch \
+  dosbox-sdl2 \
+  moonlight-embedded \
+  libretro-2048 \
+  libretro-beetle-wswan \
+  libretro-fbalpha \
+  libretro-fuse \
+  libretro-genplus \
+  libretro-mame2003-plus \
+  libretro-mgba \
+  libretro-mrboom \
+  libretro-mupen64plus \
+  libretro-pcsx-rearmed \
+  libretro-prboom \
+  libretro-quicknes \
+  libretro-samples \
+  libretro-scummvm \
+  libretro-tyrquake"
+
+OEM_EMU_GENERIC=" \
+  dolphin \
+  fs-uae \
+  ppsspp \
+  vice \
+  libretro-beetle-psx \
+  libretro-beetle-saturn \
+  libretro-citra \
+  libretro-desmume \
+  libretro-dolphin \
+  libretro-mame2016 \
+  libretro-parallel-n64 \
+  libretro-reicast \
+  libretro-snes9x"
+
+OEM_EMU_RPI=" \
+  amiberry \
+  libretro-snes9x2010 \
+  libretro-vice \
+  libretro-yabause"
+
+if [ ! "$OEM_APPS" = "no" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_APPS_COMMON"
+  case $PROJECT in
+    Generic)
+      PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_APPS_GENERIC"
+      ;;
+    RPi*)
+      PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_APPS_RPI"
+      ;;
+  esac
+fi
+
+if [ ! "$OEM_EMU" = "no" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_COMMON"
+  case $PROJECT in
+    Generic)
+      PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_GENERIC"
+      ;;
+    RPi*)
+      PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_RPI"
+      ;;
+  esac
+fi
+
+makeinstall_target() {
+  mkdir -p $INSTALL
+  cp -PR $PKG_DIR/files/* $INSTALL
+}
