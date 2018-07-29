1fd352c380399f4dd3bd341553fd0fc037ca2083
5schatten <f.hartung@outlook.de>
2018-07-29 14:26:03 +0200

    libretro-citra: initial package git 8dc3431

---
+++ packages/5schatten/emulation-cores/libretro-citra/package.mk
@@ -0,0 +1,42 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="libretro-citra"
+PKG_VERSION="8dc3431"
+PKG_ARCH="x86_64"
+PKG_LICENSE="GPLv2+"
+PKG_SITE="https://github.com/libretro/citra"
+PKG_URL="https://github.com/libretro/citra.git"
+PKG_DEPENDS_TARGET="toolchain boost"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="A Nintendo 3DS Emulator"
+GET_HANDLER_SUPPORT="git"
+PKG_TOOLCHAIN="cmake-make"
+
+PKG_LIBNAME="citra_libretro.so"
+PKG_LIBPATH="src/citra_libretro/$PKG_LIBNAME"
+PKG_LIBVAR="CITRA_LIB"
+
+PKG_CMAKE_OPTS_TARGET="-DENABLE_LIBRETRO=1 \
+                       -DENABLE_SDL2=0 \
+                       -DENABLE_QT=0 \
+                       -DCMAKE_BUILD_TYPE=\"Release\" \
+                       -DENABLE_WEB_SERVICE=0 \
+                       -DCMAKE_NO_SYSTEM_FROM_IMPORTED=1 \
+                       -DCMAKE_VERBOSE_MAKEFILE=1 \
+                       --target citra_libretro"
+
+pre_make_target() {
+  find . -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+}
+
+makeinstall_target() {
+  if [ ! "$OEM_EMU" = "no" ]; then
+    mkdir -p $INSTALL/usr/lib/libretro
+    cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  fi
+ 
+  mkdir -p $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME
+  cp $PKG_LIBPATH $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME
+  echo "set($PKG_LIBVAR $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME/$PKG_NAME-config.cmake
+}
