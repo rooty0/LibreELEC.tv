028cbba96280dca0911232975848948a198abc86
5schatten <f.hartung@outlook.de>
2018-07-29 14:36:01 +0200

    libretro-samples: initial package git 6b90ce9

---
+++ packages/5schatten/emulation-frontends/libretro-samples/package.mk
@@ -0,0 +1,43 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="libretro-samples"
+PKG_VERSION="6b90ce9"
+PKG_SHA256="2bd1b3af783a028355eb4b4c416f09802313a9a69759e716377b55012f7bb4ae"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/libretro/libretro-samples"
+PKG_URL="https://github.com/libretro/libretro-samples/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="A set of samples to illustrate libretro API."
+PKG_TOOLCHAIN="manual"
+
+make_target() {
+ make -C input/button_test
+ make -C tests/test
+ make -C tests/test_advanced
+
+ if [ "$ARCH" == "x86_64" ]; then
+   make -C video/opengl/libretro_test_gl_fixedfunction
+   make -C video/opengl/libretro_test_gl_shaders
+   make -C video/vulkan/vk_rendering
+   make -C video/vulkan/vk_async_compute
+ fi
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+
+  cp input/button_test/*.so $INSTALL/usr/lib/libretro/
+
+  cp tests/test/*.so $INSTALL/usr/lib/libretro/
+  cp tests/test_advanced/*.so $INSTALL/usr/lib/libretro/
+
+  if [ "$ARCH" == "x86_64" ]; then
+    cp video/opengl/libretro_test_gl_fixedfunction/*.so $INSTALL/usr/lib/libretro/
+    cp video/opengl/libretro_test_gl_shaders/*.so $INSTALL/usr/lib/libretro/
+    cp video/vulkan/vk_rendering/*.so $INSTALL/usr/lib/libretro/
+    cp video/vulkan/vk_async_compute/*.so $INSTALL/usr/lib/libretro/
+  fi
+}
