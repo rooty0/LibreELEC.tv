d1bcd88d8bec5708f91b8f079486bbd04ae517ee
5schatten <f.hartung@outlook.de>
2018-07-29 14:14:50 +0200

    SDL2_image: initial package v2.0.3

---
+++ packages/5schatten/depends/SDL2_image/package.mk
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="SDL2_image"
+PKG_VERSION="2.0.3"
+PKG_SHA256="3510c25da735ffcd8ce3b65073150ff4f7f9493b866e85b83738083b556d2368"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.libsdl.org/"
+PKG_URL="https://www.libsdl.org/projects/SDL_image/release/SDL2_image-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain SDL2"
+PKG_SECTION="emulation/depends"
+PKG_SHORTDESC="SDL_image is an image file loading library. "
+PKG_LONGDESC="SDL_image is an image loading library that is used with the SDL library, and almost as portable. It allows a programmer to use multiple image formats without having to code all the loading and conversion algorithms themselves."
