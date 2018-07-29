26be98ba28571556d38b06f0478e35b48a041675
5schatten <f.hartung@outlook.de>
2018-07-29 14:35:04 +0200

    libretro-core-info: initial package git 45ca4fd

---
+++ packages/5schatten/emulation-frontends/libretro-core-info/package.mk
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="libretro-core-info"
+PKG_VERSION="45ca4fd"
+PKG_SHA256="d5add0a4daf27f70fcd71f43676a747051ec1088a28cf0ba4ffbf7bac2db9a0c"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/libretro/libretro-core-info"
+PKG_URL="https://github.com/libretro/libretro-core-info/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emulation"
+PKG_LONGDESC="Mirror of libretro's core info files"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp $PKG_BUILD/*.info $INSTALL/usr/lib/libretro/
+}
+
