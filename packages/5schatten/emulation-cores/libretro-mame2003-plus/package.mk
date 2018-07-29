0fbbec05ec0c4461c9e2d528264d502ec24949b0
5schatten <f.hartung@outlook.de>
2018-07-29 14:26:28 +0200

    libretro-mame2003-plus: initial package git e166e2f

---
+++ packages/5schatten/emulation-cores/libretro-mame2003-plus/package.mk
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="libretro-mame2003-plus"
+PKG_VERSION="e166e2f"
+PKG_SHA256="c64ecd7b39a1885c6fa6c4833b30fb5fda6ae73d1cd91ae7c504cf5a1ce285db"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/mame2003-plus-libretro"
+PKG_URL="https://github.com/libretro/mame2003-plus-libretro/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="mame2003-plus-libretro-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emulation"
+PKG_LONGDESC="Updated 2018 version of MAME (0.78) for libretro. with added game support plus many fixes and improvements"
+
+PKG_LIBNAME="mame2003_plus_libretro.so"
+PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBVAR="MAME2003PLUS_LIB"
+
+configure_target() {
+  export LD="$CC"
+}
+
+make_target() {
+  make GIT_VERSION=$PKG_VERSION
+}
+
+makeinstall_target() {
+  if [ ! "$OEM_EMU" = "no" ]; then
+    mkdir -p $INSTALL/usr/lib/libretro
+    cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  fi
+}
