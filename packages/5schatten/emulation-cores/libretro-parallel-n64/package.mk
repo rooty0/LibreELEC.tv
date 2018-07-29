f506f587af8ed8ffa5c3ff948ce1394e8df09618
5schatten <f.hartung@outlook.de>
2018-07-29 14:27:05 +0200

    libretro-parallel-n64: initial package git 40469f0

---
+++ packages/5schatten/emulation-cores/libretro-parallel-n64/package.mk
@@ -0,0 +1,45 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="libretro-parallel-n64"
+PKG_VERSION="40469f0"
+PKG_SHA256="c5d18257dee112b41537bcf21ecbde23c10e856f9b84926afef7df5e5c50e8d2"
+PKG_ARCH="any"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/parallel-n64"
+PKG_URL="https://github.com/libretro/parallel-n64/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="parallel-n64-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="Optimized/rewritten Nintendo 64 emulator made specifically for Libretro. Originally based on Mupen64 Plus."
+
+PKG_LIBNAME="parallel_n64_libretro.so"
+PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBVAR="PARALLELN64_LIB"
+
+pre_build_target() {
+  export GIT_VERSION=$PKG_VERSION
+}
+
+make_target() {
+  if [ -z "$DEVICE" ]; then
+    PKG_DEVICE_NAME=$PROJECT
+  else
+    PKG_DEVICE_NAME=$DEVICE
+  fi
+
+  case $PKG_DEVICE_NAME in
+    RPi|RPi2)
+      make platform=${PKG_DEVICE_NAME,,}
+      ;;
+    Generic)
+      make WITH_DYNAREC=$ARCH
+      ;;
+  esac
+}
+
+makeinstall_target() {
+    mkdir -p $INSTALL/usr/lib/libretro
+    cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+}
