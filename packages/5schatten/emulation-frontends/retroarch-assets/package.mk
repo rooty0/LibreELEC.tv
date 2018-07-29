24e41eb19ad3237c183dbe1dd5f1b2e54c49e4c4
5schatten <f.hartung@outlook.de>
2018-07-29 14:37:38 +0200

    retroarch-assets: initial package git 69c6e99

---
+++ packages/5schatten/emulation-frontends/retroarch-assets/package.mk
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 0riginally created by Escalade (https://github.com/escalade)
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="retroarch-assets"
+PKG_VERSION="69c6e99"
+PKG_SHA256="e23b5cc1bd1c8f1497cfd8384d88f88c5a49023fdf345065a1f8a8403b3eefa6"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/libretro/retroarch-assets"
+PKG_URL="https://github.com/libretro/retroarch-assets/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="RetroArch assets"
+PKG_LONGDESC="RetroArch assets. Background and icon themes for the menu drivers."
+PKG_TOOLCHAIN="manual"
+
+pre_configure_target() {
+  cd ../
+  rm -rf .$TARGET_NAME
+}
+
+makeinstall_target() {
+  make install DESTDIR=$INSTALL PREFIX=/usr
+}
+
+post_makeinstall_target() {
+  mv $INSTALL/usr/share/libretro $INSTALL/usr/share/retroarch
+}
