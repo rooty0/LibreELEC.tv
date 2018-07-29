751676e3120cd9c03b135407c1f034a322aba2a3
5schatten <f.hartung@outlook.de>
2018-07-29 14:36:17 +0200

    libretro-slang-shaders: initial package git 33129be

---
+++ packages/5schatten/emulation-frontends/libretro-slang-shaders/package.mk
@@ -0,0 +1,25 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="libretro-slang-shaders"
+PKG_VERSION="33129be"
+PKG_SHA256="e5edd6a02b897319a480407c2217a8482cc581f195ad15689ff7f84a1785d61d"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/libretro/slang-shaders"
+PKG_URL="https://github.com/libretro/slang-shaders/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="slang-shaders-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="Vulkan GLSL RetroArch shader system"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  cd $PKG_BUILD
+  find . -type f -exec chmod 644 {} \;
+  cd -
+  mkdir -p $INSTALL/usr/share/retroarch/shaders
+  cp -R $PKG_BUILD/* $INSTALL/usr/share/retroarch/shaders
+  rm -r $INSTALL/usr/share/retroarch/shaders/configure
+  rm -r $INSTALL/usr/share/retroarch/shaders/Makefile
+}
