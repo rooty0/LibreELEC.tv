db8e413f6d4e0a1b7fe8d28d80df8deff8f98fd8
5schatten <f.hartung@outlook.de>
2018-08-02 12:37:43 +0200

    glfw: update to v3.2.1

--- packages/graphics/glfw/package.mk
+++ packages/graphics/glfw/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="glfw"
-PKG_VERSION="2.7.9"
-PKG_SHA256="b7276dcadc85a07077834d1043f11ffd6a3a379647bb94361b4abc3ffca75e7d"
+PKG_VERSION="3.2.1"
+PKG_SHA256="e10f0de1384d75e6fc210c53e91843f6110d6c4f3afbfb588130713c2f9d8fe8"
 PKG_ARCH="x86_64"
 PKG_LICENSE="BSD"
 PKG_SITE="http://glfw.org"
-PKG_URL="$SOURCEFORGE_SRC/glfw/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="https://github.com/glfw/glfw/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain mesa glu"
 PKG_SECTION="graphics"
 PKG_SHORTDESC="provides a simple API for creating windows, contexts and surfaces, receiving input and events"
@@ -17,10 +17,14 @@ if [ "$OPENGL" = "no" ] ; then
   exit 0
 fi
 
-make_target() {
-  make x11 PREFIX=$SYSROOT_PREFIX/usr
-}
+PKG_CMAKE_OPTS_TARGET="-DGLFW_BUILD_EXAMPLES=off \
+                       -DGLFW_BUILD_TESTS=off \
+                       -DGLFW_BUILD_DOCS=off"
 
-makeinstall_target() {
-  make x11-install PREFIX=$SYSROOT_PREFIX/usr
-}
+#make_target() {
+#  make x11 PREFIX=$SYSROOT_PREFIX/usr
+#}
+
+#makeinstall_target() {
+#  make x11-install PREFIX=$SYSROOT_PREFIX/usr
+#}
