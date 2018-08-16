86bdfd3df6c11fa188fd0c35e6960d9e2efca4ba
5schatten <f.hartung@outlook.de>
2018-08-16 23:08:53 +0200

    sdl-jstest: update to v0.2.1 / changed pkg host to gitlab

--- packages/5schatten/sysutils/sdl-jstest/package.mk
+++ packages/5schatten/sysutils/sdl-jstest/package.mk
@@ -3,15 +3,18 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="sdl-jstest"
-PKG_VERSION="34042f6"
-PKG_SHA256="054dcd5a673147c6119b93910040fe8556839f3af6060ffabf882753f9464eaa"
+PKG_VERSION="53b89cc2" #v0.2.1
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/Grumbel/sdl-jstest"
-PKG_URL="https://github.com/Grumbel/sdl-jstest/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://gitlab.com/sdl-jstest/sdl-jstest"
+PKG_URL="https://gitlab.com/sdl-jstest/sdl-jstest.git"
+PKG_SOURCE_DIR="$PKG_NAME-*$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain cmake:host SDL2 ncurses"
 PKG_SECTION="emulation/sysutils"
 PKG_SHORTDESC="sdl2-jstest are simple programs that lets you find out how many joysticks SDL2 detects on your system, how many axes, buttons, hats and balls they have each."
+GET_HANDLER_SUPPORT="git"
+
+PKG_CMAKE_OPTS_TARGET="-DBUILD_SDL_JSTEST=off"
 
 post_makeinstall_target() {
  ln -sf sdl2-jstest $INSTALL/usr/bin/sdl-jstest
