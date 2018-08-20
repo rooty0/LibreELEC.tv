fdf4330ebcd58d2b65c84052a572dee798dac013
5schatten <f.hartung@outlook.de>
2018-08-20 15:07:15 +0200

    libretro-slang-shaders: update to git d96ecca

--- packages/5schatten/emulation-frontends/libretro-slang-shaders/package.mk
+++ packages/5schatten/emulation-frontends/libretro-slang-shaders/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="libretro-slang-shaders"
-PKG_VERSION="19b1796"
-PKG_SHA256="bfb808bd1f544cde450ef50f3fa5cc23f06a068736df198faa2f0aee43ed83e3"
+PKG_VERSION="d96ecca"
+PKG_SHA256="c8b923b0b29cf6d1ef44b6e0b6b32bff1ab454f847a27fcbd67216e2ac421544"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/slang-shaders"
