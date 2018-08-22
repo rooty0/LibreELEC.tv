88cedaf2b197a90f3b99a8db21101eec88205af2
5schatten <f.hartung@outlook.de>
2018-08-22 13:38:03 +0200

    emulationstation-retropie: update to git dc541e5

--- packages/5schatten/emulation-frontends/emulationstation-retropie/package.mk
+++ packages/5schatten/emulation-frontends/emulationstation-retropie/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="emulationstation-retropie"
-PKG_VERSION="de021ca"
+PKG_VERSION="dc541e5"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/RetroPie/EmulationStation"
