bdd33b84d66a210b5063b4594077dcb3e3423f9f
5schatten <f.hartung@outlook.de>
2018-09-10 14:04:09 +0200

    libretro-reicast: update to git d50d346

--- packages/emulation/libretro-reicast/package.mk
+++ packages/emulation/libretro-reicast/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-reicast"
-PKG_VERSION="29cbf0d"
-PKG_SHA256="e94eb7c9be868c33330b30b82fa18172c828ce9ffce0b87138f56439cb79aa8e"
+PKG_VERSION="d50d346"
+PKG_SHA256="e915e11bb501127e96cdc157b8c0a942f8d375ed9fa33a2bf262f8a2315c206c"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/reicast-emulator"
@@ -25,21 +25,32 @@ pre_make_target() {
 }
 
 make_target() {
-  case $DEVICE in
-    RPi)
-      make FORCE_GLES=1 HAVE_OPENMP=0 GIT_VERSION=$PKG_VERSION WITH_DYNAREC=$ARCH platform=rpi
-      ;;
-    RPi2)
-      make FORCE_GLES=1 HAVE_OPENMP=0 GIT_VERSION=$PKG_VERSION WITH_DYNAREC=$ARCH platform=rpi2
-      ;;
-  esac
+  if [ "$PROJECT" == "Amlogic" ]; then
+    make FORCE_GLES=1 HAVE_OPENMP=0 GIT_VERSION=$PKG_VERSION WITH_DYNAREC=$ARCH platform=rpi3
+  fi
 
-  if [ "$ARCH" = "x86_64" ]; then
+  if [ "$PROJECT" == "RPi" ]; then
+    case $DEVICE in
+      RPi)
+        make FORCE_GLES=1 HAVE_OPENMP=0 GIT_VERSION=$PKG_VERSION WITH_DYNAREC=$ARCH platform=rpi
+        ;;
+      RPi2)
+        make FORCE_GLES=1 HAVE_OPENMP=0 GIT_VERSION=$PKG_VERSION WITH_DYNAREC=$ARCH platform=rpi2
+        ;;
+    esac
+  fi
+
+  if [ "$PROJECT" == "Generic" ]; then
+      #clean build dir
       rm -rf tmp/*.so
       mkdir -p tmp
+
+      #build reicast oit
       make AS=${AS} CC_AS=${AS} HAVE_OPENMP=0 GIT_VERSION=$PKG_VERSION
       mv *.so tmp
       make clean
+
+      #build reicast oit
       make AS=${AS} CC_AS=${AS} HAVE_OPENMP=0 HAVE_OIT=1 GIT_VERSION=$PKG_VERSION WITH_DYNAREC=$ARCH
       mv tmp/*.so .
   fi
