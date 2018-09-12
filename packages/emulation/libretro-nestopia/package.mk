297d787b2315582ebaceef5db3ae34d777658c1b
5schatten <f.hartung@outlook.de>
2018-09-12 12:04:58 +0200

    libretro-nestopia: update to git faf19f8

--- packages/emulation/libretro-nestopia/package.mk
+++ packages/emulation/libretro-nestopia/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-nestopia"
-PKG_VERSION="9c33552"
-PKG_SHA256="b75bb329ab1396802c24108d78ec6a896a776d328e6480e47ec6c1bcc696d80d"
+PKG_VERSION="faf19f8"
+PKG_SHA256="b22a362ddd3ae25151e53ed3ae0dc429cd402ba99ac43f08d061b5def0c807ce"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/nestopia"
@@ -24,10 +24,20 @@ post_unpack() {
 }
 
 make_target() {
-  make -C libretro
+
+  if [ "$PROJECT" == "RPi" ]; then
+    make -C libretro platform=rpi2
+  else
+    make -C libretro
+  fi
 }
 
 makeinstall_target() {
+  if [ ! "$OEM_EMU" = "no" ]; then
+    mkdir -p $INSTALL/usr/lib/libretro
+    cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  fi
+
   mkdir -p $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME
   cp $PKG_LIBPATH $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME
   echo "set($PKG_LIBVAR $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME/$PKG_NAME-config.cmake
