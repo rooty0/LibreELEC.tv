33bce2b011a4a44ec59f2f38915400fb44988a12
5schatten <f.hartung@outlook.de>
2018-09-18 21:40:23 +0200

    libretro-chailove: initial package git 032b4e5

---
+++ packages/5schatten/emulation-cores/libretro-chailove/package.mk
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="libretro-chailove"
+PKG_VERSION="032b4e5"
+PKG_ARCH="any"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/libretro/libretro-chailove"
+PKG_URL="https://github.com/libretro/libretro-chailove.git"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emulation"
+PKG_LONGDESC="ChaiLove is an awesome framework you can use to make 2D games in ChaiScript."
+GET_HANDLER_SUPPORT="git"
+PKG_TOOLCHAIN="manual"
+
+PKG_LIBNAME="chailove_libretro.so"
+PKG_LIBPATH="$PKG_LIBNAME"
+PKG_LIBVAR="CHAILOVE_LIB"
+
+configure_target() {
+  cd $PKG_BUILD
+}
+
+make_target() {
+  make
+}
+
+makeinstall_target() {
+  if [ "$OEM_EMU" == "yes" ]; then
+    mkdir -p $INSTALL/usr/lib/libretro
+    cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  fi
+}
