3cbe0eb5a954cad15e81c38023c6625aaa97e4ea
5schatten <f.hartung@outlook.de>
2018-09-18 21:38:24 +0200

    dolphin: update to r5.0-8743

--- packages/5schatten/emulation-cores/dolphin/package.mk
+++ packages/5schatten/emulation-cores/dolphin/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="dolphin"
-PKG_VERSION="ce9e9186f715408af8d9abb60b73ccb1e4d3c474"
-PKG_SHA256="53810b3f58248066925f92f292fb31706a4b45f87f31782b99399eb7cdf5d0a6"
+PKG_VERSION="a61036abfed95e8d1e193222e7eaa23fac4c6f43"
+PKG_SHA256="6ef6ed89c5a7e0c7d66858a4690b70fc01c8cdf4aaf35c3a37603ce83ed93cb7"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/dolphin-emu/dolphin"
@@ -27,7 +27,7 @@ pre_make_target() {
   export Qt5Gui_DIR=$SYSROOT_PREFIX/usr/lib
 
   #ugly version hack
-  PKG_DOLPHIN_RELEASE="5.0-8718"
+  PKG_DOLPHIN_RELEASE="5.0-8743"
   PKG_DOLPHIN_BRANCH="Master"
   printf "#define SCM_REV_STR \""$PKG_VERSION"\"\n""#define SCM_DESC_STR \""$PKG_DOLPHIN_RELEASE"\"\n""#define SCM_BRANCH_STR \""$PKG_DOLPHIN_BRANCH"\"\n""#define SCM_IS_MASTER 0\n""#define SCM_DISTRIBUTOR_STR \"None\"\n""#define SCM_UPDATE_TRACK_STR \"\"\n" > Source/Core/Common/scmrev.h
 }
