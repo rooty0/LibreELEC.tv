fe03fd91b63be919fe69cb5c68573ea764cc8baa
5schatten <f.hartung@outlook.de>
2018-10-01 14:37:11 +0200

    spectre-meltdown-checker: update to git f5106b3

--- packages/5schatten/sysutils/spectre-meltdown-checker/package.mk
+++ packages/5schatten/sysutils/spectre-meltdown-checker/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="spectre-meltdown-checker"
-PKG_VERSION="80e0db7"
-PKG_SHA256="76ee530db7b3770a1e7a0fabea1e30c3737ee8ceb8bc085f18e7e59ea5e1772c"
+PKG_VERSION="f5106b3"
+PKG_SHA256="2914661f483a43f5310a538398bd0253ee8514f3834a9e6972a664117792b3d7"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/speed47/spectre-meltdown-checker"
