8d4c36ae7dd1f843abb7c126e39309007a66ac51
5schatten <f.hartung@outlook.de>
2018-10-01 15:13:05 +0200

    libretro-mame2010: update to git 70732f9

--- packages/emulation/libretro-mame2010/package.mk
+++ packages/emulation/libretro-mame2010/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-mame2010"
-PKG_VERSION="70732f9137f6bb2bde4014746ea8bc613173dd1e"
+PKG_VERSION="70732f9"
 PKG_SHA256="36ab11541233c9a4240baf6f0a529d8d335dce23f25b66b950e18373fd8e65fb"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame2010-libretro"
@@ -23,25 +23,30 @@ pre_make_target() {
 make_target() {
   case $TARGET_CPU in
     arm1176jzf-s)
-      make platform=armv6-hardfloat-$TARGET_CPU
+      make platform=armv6-hardfloat-$TARGET_CPU GIT_VERSION=$PKG_VERSION
       ;;
     cortex-a7|cortex-a9)
-      make platform=armv7-neon-hardfloat-$TARGET_CPU
+      make platform=armv7-neon-hardfloat-$TARGET_CPU GIT_VERSION=$PKG_VERSION
       ;;
     *cortex-a53|cortex-a17)
       if [ "$TARGET_ARCH" = "aarch64" ]; then
-        make platform=aarch64
+        make platform=aarch64 GIT_VERSION=$PKG_VERSION
       else
-        make platform=armv7-neon-hardfloat-cortex-a9
+        make platform=armv7-neon-hardfloat-cortex-a9 GIT_VERSION=$PKG_VERSION
       fi
       ;;
     x86-64)
-      make
+      make GIT_VERSION=$PKG_VERSION
       ;;
   esac
 }
 
 makeinstall_target() {
+  if [ ! "$OEM_EMU" = "no" ]; then
+    mkdir -p $INSTALL/usr/lib/libretro
+    cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  fi
+
   mkdir -p $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME
   cp $PKG_LIBPATH $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME
   echo "set($PKG_LIBVAR $SYSROOT_PREFIX/usr/lib/$PKG_LIBNAME)" > $SYSROOT_PREFIX/usr/lib/cmake/$PKG_NAME/$PKG_NAME-config.cmake
