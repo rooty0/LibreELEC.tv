ff41db7fe73dccab216de1acf87c8803b7bd71da
5schatten <supervisedthinking@gmail.com>
2018-10-02 15:08:47 +0200

    libretro-mgba: update to git 426934d

--- packages/emulation/libretro-mgba/package.mk
+++ packages/emulation/libretro-mgba/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-mgba"
-PKG_VERSION="3fbc461"
-PKG_SHA256="21604c3f7d057666bfdd4f575a2ee6e5de301a2b91d082b4e9a2f91cd5c6f504"
+PKG_VERSION="426934d"
+PKG_SHA256="ff57437e0cba1f731145463e137c664d80edc5c487ca70c14fefc047cccba957"
 PKG_ARCH="any"
 PKG_LICENSE="MPL 2.0"
 PKG_SITE="https://github.com/libretro/mgba"
