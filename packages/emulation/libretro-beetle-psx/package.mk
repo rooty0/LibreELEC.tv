a263aad8b4a8910d05a1a3135718801588b16c72
5schatten <supervisedthinking@gmail.com>
2018-10-07 12:47:05 +0200

    libretro-beetle-psx: update to git 528036a

--- packages/emulation/libretro-beetle-psx/package.mk
+++ packages/emulation/libretro-beetle-psx/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="libretro-beetle-psx"
-PKG_VERSION="70449aa"
-PKG_SHA256="4cb52d2b10ad177ac3e63e187e95793a62a5993e210361008a2630e8af783ad7"
+PKG_VERSION="528036a"
+PKG_SHA256="fc556e263e08d5a63800a0f120e26bb408085437330c16268666d2c86c536e53"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-psx-libretro"
@@ -26,7 +26,7 @@ make_target() {
   make
 
   if [ "$PROJECT" == "Generic" ];then
-    mkdir tmp
+    mkdir -p tmp
     mv $PKG_LIBNAME tmp/
     make clean
     make HAVE_HW=1
