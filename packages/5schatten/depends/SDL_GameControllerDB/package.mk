88aab244c60aad4f66ee7f22ce6ca61fc93b74b8
5schatten <supervisedthinking@gmail.com>
2018-10-08 21:20:27 +0200

    SDL_GameControllerDB: inital package / git version 1994469

---
+++ packages/5schatten/depends/SDL_GameControllerDB/package.mk
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="SDL_GameControllerDB"
+PKG_VERSION="1994469"
+PKG_SHA256="81323414e1de5fc4402b69cd8e520595a115ec8bbc337639c7b8ffa95f1f1eb4"
+PKG_ARCH="any"
+PKG_LICENSE="OSS"
+PKG_SITE="https://github.com/gabomdq/SDL_GameControllerDB"
+PKG_URL="https://github.com/gabomdq/SDL_GameControllerDB/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET=""
+PKG_SECTION="depends/emulation"
+PKG_LONGDESC="A community sourced database of game controller mappings to be used with SDL2 Game Controller functionality"
+PKG_TOOLCHAIN="manual"
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/config/SDL-GameControllerDB
+  cp $PKG_BUILD/gamecontrollerdb.txt $INSTALL/usr/config/SDL-GameControllerDB
+}
