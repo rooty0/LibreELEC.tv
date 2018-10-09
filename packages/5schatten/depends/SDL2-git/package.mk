f322f43a67fbdc675c8954d4a58e46a97cb1efac
5schatten <supervisedthinking@gmail.com>
2018-10-09 12:24:50 +0200

    SDL2-git: initial package git 06504eb / 2.0.9-dev

---
+++ packages/5schatten/depends/SDL2-git/package.mk
@@ -0,0 +1,125 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="SDL2-git"
+PKG_VERSION="06504eb"
+PKG_SHA256="0595f8b304c2c7215f952df7d1250bd499b8567ee2653b6e69236436744a9b9a"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://www.libsdl.org/"
+PKG_URL="https://github.com/spurious/SDL-mirror/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain yasm:host alsa-lib systemd dbus"
+PKG_SECTION="multimedia"
+PKG_SHORTDESC="SDL2: A cross-platform Graphic API"
+PKG_LONGDESC="Simple DirectMedia Layer is a cross-platform multimedia library designed to provide fast access to the graphics framebuffer and audio device. It is used by MPEG playback software, emulators, and many popular games, including the award winning Linux port of 'Civilization: Call To Power.' Simple DirectMedia Layer supports Linux, Win32, BeOS, MacOS, Solaris, IRIX, and FreeBSD."
+
+# X11 Support
+if [ "$DISPLAYSERVER" = "x11" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libX11 libXrandr"
+  SUPPORT_X11="-DVIDEO_X11=ON \
+               -DX11_SHARED=ON \
+               -DVIDEO_X11_XCURSOR=OFF \
+               -DVIDEO_X11_XINERAMA=OFF \
+               -DVIDEO_X11_XINPUT=OFF \
+               -DVIDEO_X11_XRANDR=ON \
+               -DVIDEO_X11_XSCRNSAVER=OFF \
+               -DVIDEO_X11_XSHAPE=OFF \
+               -DVIDEO_X11_XVM=OFF"
+else
+  SUPPORT_X11="-DVIDEO_X11=OFF"
+fi
+
+# OpenGL Support
+if [ ! "$OPENGL" = "no" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OPENGL"
+  SUPPORT_OPENGL="-DVIDEO_OPENGL=ON"
+else
+  SUPPORT_OPENGL="-DVIDEO_OPENGL=OFF"
+fi
+
+# OpenGLES Support
+if [ ! "$OPENGLES" = "no" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OPENGLES"
+  SUPPORT_OPENGLES="-DVIDEO_OPENGLES=ON"
+else
+  SUPPORT_OPENGLES="-DVIDEO_OPENGLES=OFF"
+fi
+
+# RPi Video Support
+if [ "$OPENGLES" = "bcm2835-driver" ]; then
+  SUPPORT_RPI="-DVIDEO_RPI=ON \
+               -DVIDEO_VULKAN=OFF \
+               -DVIDEO_KMSDRM=OFF"
+else
+  SUPPORT_RPI="-DVIDEO_RPI=OFF"
+fi
+
+# AML Mali Video Support
+if [ "$OPENGLES" = "opengl-meson" ] || [ "$OPENGLES" == "opengl-meson-t82x" ]; then
+  SUPPORT_MALI="-DVIDEO_MALI=ON \
+                -DVIDEO_VULKAN=OFF \
+                -DVIDEO_KMSDRM=OFF"
+else
+  SUPPORT_MALI="-DVIDEO_MALI=OFF"
+fi
+
+# Pulseaudio Support
+if [ "$PULSEAUDIO_SUPPORT" = yes ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET pulseaudio"
+
+  SUPPORT_PULSEAUDIO="-DPULSEAUDIO=ON \
+                      -DPULSEAUDIO_SHARED=ON"
+else
+  SUPPORT_PULSEAUDIO="-DPULSEAUDIO=OFF \
+                      -DPULSEAUDIO_SHARED=OFF"
+fi
+
+PKG_CMAKE_OPTS_TARGET="-DSDL_STATIC=OFF \
+                       -DLIBC=ON \
+                       -DGCC_ATOMICS=ON \
+                       -DASSEMBLY=ON \
+                       -DALTIVEC=OFF \
+                       -DOSS=OFF \
+                       -DALSA=ON \
+                       -DALSA_SHARED=ON \
+                       -DESD=OFF \
+                       -DESD_SHARED=OFF \
+                       -DARTS=OFF \
+                       -DARTS_SHARED=OFF \
+                       -DNAS=OFF \
+                       -DNAS_SHARED=ON \
+                       -DSNDIO=OFF \
+                       -DDISKAUDIO=OFF \
+                       -DDUMMYAUDIO=OFF \
+                       -DVIDEO_WAYLAND=OFF \
+                       -DVIDEO_WAYLAND_QT_TOUCH=ON \
+                       -DWAYLAND_SHARED=OFF \
+                       -DVIDEO_MIR=OFF \
+                       -DMIR_SHARED=OFF \
+                       -DVIDEO_COCOA=OFF \
+                       -DVIDEO_DIRECTFB=OFF \
+                       -DVIDEO_VIVANTE=OFF \
+                       -DDIRECTFB_SHARED=OFF \
+                       -DFUSIONSOUND=OFF \
+                       -DFUSIONSOUND_SHARED=OFF \
+                       -DVIDEO_DUMMY=OFF \
+                       -DINPUT_TSLIB=OFF \
+                       -DPTHREADS=ON \
+                       -DPTHREADS_SEM=ON \
+                       -DDIRECTX=OFF \
+                       -DSDL_DLOPEN=ON \
+                       -DCLOCK_GETTIME=OFF \
+                       -DRPATH=OFF \
+                       -DRENDER_D3D=OFF \
+                       $SUPPORT_X11 \
+                       $SUPPORT_OPENGL \
+                       $SUPPORT_OPENGLES \
+                       $SUPPORT_RPI \
+                       $SUPPORT_MALI \
+                       $SUPPORT_PULSEAUDIO"
+
+post_makeinstall_target() {
+  $SED "s:\(['=\" ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" $SYSROOT_PREFIX/usr/bin/sdl2-config
+
+  rm -rf $INSTALL/usr/bin
+}
