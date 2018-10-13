fb1d0f084fc95ee2d0ce63f2a7799f48a5c2097a
5schatten <supervisedthinking@gmail.com>
2018-10-13 15:22:03 +0200

    dolphin: update to r5.0-8874

--- packages/5schatten/emulation-cores/dolphin/package.mk
+++ packages/5schatten/emulation-cores/dolphin/package.mk
@@ -3,13 +3,13 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="dolphin"
-PKG_VERSION="406159f7f039f9d06dc10a47061c853f9fddb583"
-PKG_SHA256="8f6840b2da237a84fb00b03451d36fd7a3a750d0b61821dff754780a70c35d32"
+PKG_VERSION="22c63433a2ec13c8e6aa17b611a84a0951d283e2"
+PKG_SHA256="cbd77368a72367ae9fef82dd3f20b7bc6f3f1988efb36802db3abd55b405cce1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/dolphin-emu/dolphin"
 PKG_URL="https://github.com/dolphin-emu/dolphin/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain cmake:host openal-soft libevdev gtk+ ffmpeg zlib bluez pulseaudio alsa-lib libogg libvorbis libSM enet qt-everywhere vulkan-loader"
+PKG_DEPENDS_TARGET="toolchain openal-soft libevdev gtk+ ffmpeg zlib bluez pulseaudio alsa-lib libogg libvorbis libSM enet qt-everywhere vulkan-loader"
 PKG_SECTION="emulation"
 PKG_SHORTDESC="Dolphin GameCube/Wii emulator"
 PKG_TOOLCHAIN="cmake-make"
@@ -27,7 +27,7 @@ pre_make_target() {
   export Qt5Gui_DIR=$SYSROOT_PREFIX/usr/lib
 
   #ugly version hack
-  PKG_DOLPHIN_RELEASE="5.0-8806"
+  PKG_DOLPHIN_RELEASE="5.0-8874"
   PKG_DOLPHIN_BRANCH="Master"
   printf "#define SCM_REV_STR \""$PKG_VERSION"\"\n""#define SCM_DESC_STR \""$PKG_DOLPHIN_RELEASE"\"\n""#define SCM_BRANCH_STR \""$PKG_DOLPHIN_BRANCH"\"\n""#define SCM_IS_MASTER 0\n""#define SCM_DISTRIBUTOR_STR \"None\"\n""#define SCM_UPDATE_TRACK_STR \"\"\n" > Source/Core/Common/scmrev.h
 }
