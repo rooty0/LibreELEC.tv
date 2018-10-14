2ee93ced761aa0c85d4582bf5d033e754f05b77a
5schatten <supervisedthinking@gmail.com>
2018-10-14 22:51:19 +0200

    citra: update to git 84ddb1c

--- packages/5schatten/emulation-cores/citra/package.mk
+++ packages/5schatten/emulation-cores/citra/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="citra"
-PKG_VERSION="0962ab8"
+PKG_VERSION="84ddb1c"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2+"
 PKG_SITE="https://github.com/citra-emu/citra"
