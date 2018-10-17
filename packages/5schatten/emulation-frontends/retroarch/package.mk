87008e0af854958dcdf54ceaac2c0bfc779830de
5schatten <supervisedthinking@gmail.com>
2018-10-18 00:10:29 +0200

    retroarch: updated to git aaafa18 / 1.7.6-dev

--- packages/5schatten/emulation-frontends/retroarch/package.mk
+++ packages/5schatten/emulation-frontends/retroarch/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="retroarch"
-PKG_VERSION="84948c9" #1.7.6-dev
+PKG_VERSION="aaafa18" #1.7.6-dev 
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch"
