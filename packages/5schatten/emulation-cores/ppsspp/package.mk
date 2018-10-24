e2c66073e5c26013eb0f5ef39c4bed09a6243ae1
5schatten <supervisedthinking@gmail.com>
2018-10-24 16:59:19 +0200

    ppsspp: update to git 6c84f28

--- packages/5schatten/emulation-cores/ppsspp/package.mk
+++ packages/5schatten/emulation-cores/ppsspp/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="ppsspp"
-PKG_VERSION="8883156"
+PKG_VERSION="6c84f28"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/hrydgard/ppsspp"
 PKG_URL="https://github.com/hrydgard/ppsspp.git"
