07cd6db11486fd11185a4a165e3aeb2cc074eb04
5schatten <supervisedthinking@gmail.com>
2018-10-27 13:58:51 +0200

    lr-common-shaders: update to git 39a15c0

--- packages/5schatten/emulation-frontends/lr-common-shaders/package.mk
+++ packages/5schatten/emulation-frontends/lr-common-shaders/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-common-shaders"
-PKG_VERSION="5e14fb8"
-PKG_SHA256="3a972f4c44ae625a4901a5a752f8a4668791de5e48167293683898e82e9bd41d"
+PKG_VERSION="39a15c02b777111fecdc9e1b38843596762ee28b"
+PKG_SHA256="3cbb35d9340fb5f47864f8fec320ab28b5d75ac72f3192460b8f958b4d6a55a4"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/libretro/common-shaders"
 PKG_URL="https://github.com/libretro/common-shaders/archive/$PKG_VERSION.tar.gz"
