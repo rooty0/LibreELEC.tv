a7a8e5ca560ef4bff93b86e343b18eedb29baabf
5schatten <supervisedthinking@gmail.com>
2018-11-11 01:18:14 +0100

    makemkv-bin: update to v1.14.1

--- packages/5schatten/depends/makemkv-bin/package.mk
+++ packages/5schatten/depends/makemkv-bin/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="makemkv-bin"
-PKG_VERSION="1.14.0"
-PKG_SHA256="2e7c7eaf4e0544f19e2ac630b1787fde2811ad60f6b4e3093875386938bfa5f6"
+PKG_VERSION="1.14.1"
+PKG_SHA256="17e62cf59c9a8e1e4535d6a6a86cc1b9f6f6c71e86dff8a5ddf38551c2928fd8"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.makemkv.com/"
