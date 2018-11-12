7c208ded4803fb13d20e86f6e46784442bbac4bd
5schatten <supervisedthinking@gmail.com>
2018-11-12 16:34:20 +0100

    ppsspp: updated to v1.7.1 / reworked cmake opts

--- packages/5schatten/emulation-cores/ppsspp/package.mk
+++ packages/5schatten/emulation-cores/ppsspp/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="ppsspp"
-PKG_VERSION="caa506b" #v1.7.0
+PKG_VERSION="fdb07323e0878e4773011a7a51a43a8900ad9d4a" #v1.7.1
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/hrydgard/ppsspp"
 PKG_URL="https://github.com/hrydgard/ppsspp.git"
@@ -14,27 +14,35 @@ PKG_TOOLCHAIN="cmake-make"
 PKG_LIBNAME="ppsspp_libretro.so"
 PKG_LIBPATH="lib/$PKG_LIBNAME"
 
-if [ "$PROJECT" = "Amlogic" ] || [ "$PROJECT" = "RPi" ]; then
-  case $DEVICE in
-     KVIM|RPi2|S905|Odroid_C2)
-      ARCH_ARM="-DARMV7=ON \
-                -DUSING_FBDEV=ON \
-                -DUSING_EGL=ON \
-                -DUSING_GLES2=ON \
-                -DUSING_X11_VULKAN=OFF"
-     ;;
-  esac
+if [ $ARCH = "arm" ] && [ ! $TARGET_CPU = "arm1176jzf-s" ]; then
+  PPSSPP_ARCH_ARM="-DARMV7=ON"
+elif [ $TARGET_CPU = "arm1176jzf-s" ]; then
+  PPSSPP_ARCH_ARM="-DARM=ON"
+fi
+
+if [ $OPENGLES_SUPPORT = "yes" ]; then
+  PPSSPP_OPENGLES_SUPPORT="-DUSING_FBDEV=ON \
+                           -DUSING_EGL=ON \
+                           -DUSING_GLES2=ON"
+fi
+
+if [ "DISPLAYSERVER" = "x11" ] && [ $VULKAN_SUPPORT = "yes" ]; then
+  PPSSPP_VULKAN_SUPPORT="-DUSING_X11_VULKAN=ON"
+else
+  PPSSPP_VULKAN_SUPPORT="-DUSING_X11_VULKAN=OFF"
 fi
 
 PKG_CMAKE_OPTS_TARGET="-DLIBRETRO=ON \
                        -DUSE_SYSTEM_FFMPEG=ON \
-                       $ARCH_ARM"
+                       $PPSSPP_ARCH_ARM \
+                       $PPSSPP_OPENGLES_SUPPORT \
+                       $PPSSPP_VULKAN_SUPPORT"
 
 pre_make_target() {
   find . -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
 }
 
 makeinstall_target() {
-    mkdir -p $INSTALL/usr/lib/libretro
-    cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
 }
