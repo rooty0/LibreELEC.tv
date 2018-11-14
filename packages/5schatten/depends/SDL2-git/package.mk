3f3dc5f63385f9376ac23a916767d9767569677c
5schatten <supervisedthinking@gmail.com>
2018-11-14 22:57:13 +0100

    SDL2-git: updated cmake opts

--- packages/5schatten/depends/SDL2-git/package.mk
+++ packages/5schatten/depends/SDL2-git/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="SDL2-git"
-PKG_VERSION="4cd4ad8df6db00989dad155d43a3f13f9ba22f10" # 2.0.9
+PKG_VERSION="4cd4ad8df6db00989dad155d43a3f13f9ba22f10" # 2.0.9+
 PKG_SHA256="b91987e4bd2a3797a842c8a1ee00bd7a7040f419f6e8c0f888689102e8c44930"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libsdl.org/"
@@ -13,62 +13,62 @@ PKG_LONGDESC="SDL2: A cross-platform Graphic API"
 # X11 Support
 if [ "$DISPLAYSERVER" = "x11" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET libX11 libXrandr"
-  SUPPORT_X11="-DVIDEO_X11=ON \
-               -DX11_SHARED=ON \
-               -DVIDEO_X11_XCURSOR=OFF \
-               -DVIDEO_X11_XINERAMA=OFF \
-               -DVIDEO_X11_XINPUT=OFF \
-               -DVIDEO_X11_XRANDR=ON \
-               -DVIDEO_X11_XSCRNSAVER=OFF \
-               -DVIDEO_X11_XSHAPE=OFF \
-               -DVIDEO_X11_XVM=OFF"
+  SDL2_SUPPORT_X11="-DVIDEO_X11=ON \
+                    -DX11_SHARED=ON \
+                    -DVIDEO_X11_XCURSOR=OFF \
+                    -DVIDEO_X11_XINERAMA=OFF \
+                    -DVIDEO_X11_XINPUT=OFF \
+                    -DVIDEO_X11_XRANDR=ON \
+                    -DVIDEO_X11_XSCRNSAVER=OFF \
+                    -DVIDEO_X11_XSHAPE=OFF \
+                    -DVIDEO_X11_XVM=OFF"
 else
-  SUPPORT_X11="-DVIDEO_X11=OFF"
+  SDL2_SUPPORT_X11="-DVIDEO_X11=OFF"
 fi
 
 # OpenGL Support
 if [ ! "$OPENGL" = "no" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OPENGL"
-  SUPPORT_OPENGL="-DVIDEO_OPENGL=ON"
+  SDL2_SUPPORT_OPENGL="-DVIDEO_OPENGL=ON"
 else
-  SUPPORT_OPENGL="-DVIDEO_OPENGL=OFF"
+  SDL2_SUPPORT_OPENGL="-DVIDEO_OPENGL=OFF"
 fi
 
 # OpenGLES Support
 if [ ! "$OPENGLES" = "no" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OPENGLES"
-  SUPPORT_OPENGLES="-DVIDEO_OPENGLES=ON"
+  SDL2_SUPPORT_OPENGLES="-DVIDEO_OPENGLES=ON"
 else
-  SUPPORT_OPENGLES="-DVIDEO_OPENGLES=OFF"
+  SDL2_SUPPORT_OPENGLES="-DVIDEO_OPENGLES=OFF"
 fi
 
 # RPi Video Support
 if [ "$OPENGLES" = "bcm2835-driver" ]; then
-  SUPPORT_RPI="-DVIDEO_RPI=ON \
-               -DVIDEO_VULKAN=OFF \
-               -DVIDEO_KMSDRM=OFF"
+  SDL2_SUPPORT_RPI="-DVIDEO_RPI=ON \
+                    -DVIDEO_VULKAN=OFF \
+                    -DVIDEO_KMSDRM=OFF"
 else
-  SUPPORT_RPI="-DVIDEO_RPI=OFF"
+  SDL2_SUPPORT_RPI="-DVIDEO_RPI=OFF"
 fi
 
 # AML Mali Video Support
 if [ "$OPENGLES" = "opengl-meson" ] || [ "$OPENGLES" = "opengl-meson-t82x" ]; then
-  SUPPORT_MALI="-DVIDEO_MALI=ON \
-                -DVIDEO_VULKAN=OFF \
-                -DVIDEO_KMSDRM=OFF"
+  SDL2_SUPPORT_MALI="-DVIDEO_MALI=ON \
+                     -DVIDEO_VULKAN=OFF \
+                     -DVIDEO_KMSDRM=OFF"
 else
-  SUPPORT_MALI="-DVIDEO_MALI=OFF"
+  SDL2_SUPPORT_MALI="-DVIDEO_MALI=OFF"
 fi
 
 # Pulseaudio Support
 if [ "$PULSEAUDIO_SUPPORT" = yes ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET pulseaudio"
 
-  SUPPORT_PULSEAUDIO="-DPULSEAUDIO=ON \
-                      -DPULSEAUDIO_SHARED=ON"
+  SDL2_SUPPORT_PULSEAUDIO="-DPULSEAUDIO=ON \
+                           -DPULSEAUDIO_SHARED=ON"
 else
-  SUPPORT_PULSEAUDIO="-DPULSEAUDIO=OFF \
-                      -DPULSEAUDIO_SHARED=OFF"
+  SDL2_SUPPORT_PULSEAUDIO="-DPULSEAUDIO=OFF \
+                           -DPULSEAUDIO_SHARED=OFF"
 fi
 
 PKG_CMAKE_OPTS_TARGET="-DSDL_STATIC=OFF \
@@ -84,7 +84,9 @@ PKG_CMAKE_OPTS_TARGET="-DSDL_STATIC=OFF \
                        -DARTS=OFF \
                        -DARTS_SHARED=OFF \
                        -DNAS=OFF \
-                       -DNAS_SHARED=ON \
+                       -DNAS_SHARED=OFF \
+                       -DLIBSAMPLERATE=OFF \
+                       -DLIBSAMPLERATE_SHARED=OFF \
                        -DSNDIO=OFF \
                        -DDISKAUDIO=OFF \
                        -DDUMMYAUDIO=OFF \
@@ -108,12 +110,12 @@ PKG_CMAKE_OPTS_TARGET="-DSDL_STATIC=OFF \
                        -DCLOCK_GETTIME=OFF \
                        -DRPATH=OFF \
                        -DRENDER_D3D=OFF \
-                       $SUPPORT_X11 \
-                       $SUPPORT_OPENGL \
-                       $SUPPORT_OPENGLES \
-                       $SUPPORT_RPI \
-                       $SUPPORT_MALI \
-                       $SUPPORT_PULSEAUDIO"
+                       $SDL2_SUPPORT_X11 \
+                       $SDL2_SUPPORT_OPENGL \
+                       $SDL2_SUPPORT_OPENGLES \
+                       $SDL2_SUPPORT_RPI \
+                       $SDL2_SUPPORT_MALI \
+                       $SDL2_SUPPORT_PULSEAUDIO"
 
 post_makeinstall_target() {
   sed -e "s:\(['=\" ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" -i $SYSROOT_PREFIX/usr/bin/sdl2-config
