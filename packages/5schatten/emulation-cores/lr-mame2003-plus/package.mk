c81ac4295fa3f176d94a23d7a280366d9e8e7511
5schatten <supervisedthinking@gmail.com>
2018-11-18 14:39:41 +0100

    lr-mame2003-plus: update to git 9670008

--- packages/5schatten/emulation-cores/lr-mame2003-plus/package.mk
+++ packages/5schatten/emulation-cores/lr-mame2003-plus/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-mame2003-plus"
-PKG_VERSION="f62b84aec571f0e7b63d088259d3b0c83db7fe9b"
-PKG_SHA256="e48b2b78a33d8af58d0590da9bbd0d69d9cb5526f002911b7b9362b1311233fc"
+PKG_VERSION="96700083abd63e5732243075f6ab06844f6156ed"
+PKG_SHA256="5010a940f9d4dc1d06217db47e7387a48bc5ec04dafec94789e21fde43889ea6"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame2003-plus-libretro"
 PKG_URL="https://github.com/libretro/mame2003-plus-libretro/archive/$PKG_VERSION.tar.gz"
