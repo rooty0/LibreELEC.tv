8fe7daa83a3a41771ac175c3ac5f1cbd06520b4d
5schatten <supervisedthinking@gmail.com>
2018-11-21 15:11:52 +0100

    retroarch-common-overlays: update to git 8570b84

--- packages/5schatten/emulation-frontends/retroarch-common-overlays/package.mk
+++ packages/5schatten/emulation-frontends/retroarch-common-overlays/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="retroarch-common-overlays"
-PKG_VERSION="779c2a5"
-PKG_SHA256="1d9374df0a6b0d59de50a0817392adc71091e6cf5e0c6ef8252e8cc264ed001b"
+PKG_VERSION="8570b844235eadfdee8b4761f6912bd97d7b53e0"
+PKG_SHA256="8d10103de2ed4a9e1a94f28a77eec01734a181f719ec1f739e8c7b08fcceee52"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/common-overlays"
 PKG_URL="https://github.com/libretro/common-overlays/archive/$PKG_VERSION.tar.gz"
