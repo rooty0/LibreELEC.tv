005816a88b44099ad23456e0b56b80bef1d2ba21
5schatten <supervisedthinking@gmail.com>
2018-11-21 16:20:35 +0100

    xf86-video-intel: update to git 746ab3b

--- packages/x11/driver/xf86-video-intel/package.mk
+++ packages/x11/driver/xf86-video-intel/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="xf86-video-intel"
-PKG_VERSION="0932a6b37ba6d5c9e916a1cb6ab89c3205b81a0c"
-PKG_SHA256="d5434256efba29328a9cd8ec20841ec158d2cf17ecaaca8458f5ca41fba245f6"
+PKG_VERSION="746ab3bb131dea8ad015e7804bfcdd6b4cc4f44e"
+PKG_SHA256="c917f86757355396f2f14f1cd39f97c4cbd33a43dd8ab9e2b10e7663ae2c8f84"
 PKG_ARCH="x86_64"
 PKG_LICENSE="OSS"
 PKG_SITE="http://intellinuxgraphics.org/"
@@ -12,7 +12,6 @@ PKG_URL="https://cgit.freedesktop.org/xorg/driver/xf86-video-intel/snapshot/$PKG
 PKG_DEPENDS_TARGET="toolchain libXcomposite libXxf86vm libXdamage libdrm util-macros systemd xorg-server"
 PKG_LONGDESC="The Xorg driver for Intel i810, i815, 830M, 845G, 852GM, 855GM, 865G, 915G, 915GM and 965G."
 PKG_TOOLCHAIN="autotools"
-# xf86-video-intel is broken enough. dont link with LTO
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-backlight \
                            --disable-backlight-helper \
