1d1c89998c8bbbaccc602ad1663821ff18b8fa21
5schatten <supervisedthinking@gmail.com>
2018-11-21 16:15:59 +0100

    retroarch: update to git b3d38c4

--- packages/5schatten/emulation-frontends/retroarch/package.mk
+++ packages/5schatten/emulation-frontends/retroarch/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="retroarch"
-PKG_VERSION="c0d5579cf25a51fdb52d3a88a06c85a6e6fcef5a" #1.7.6-dev 
+PKG_VERSION="b3d38c46aaa1cd22d7733b8967d760d9baa05402" #1.7.6-dev 
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="https://github.com/libretro/RetroArch.git"
