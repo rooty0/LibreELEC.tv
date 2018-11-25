eac3b8a78cdbe0cdb9efcbc0ae272ef44d91f661
5schatten <supervisedthinking@gmail.com>
2018-11-25 16:25:31 +0100

    lr-fceumm: update to git 684106f

--- packages/5schatten/emulation-cores/lr-fceumm/package.mk
+++ packages/5schatten/emulation-cores/lr-fceumm/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-fceumm"
-PKG_VERSION="ef60a38ef3a8fdb1bdf671e5df84ba97ff0c2220"
-PKG_SHA256="f3b0b2ce464aa595dafba880d9da2d68d6ac7b38c07edf33ae26c977c234be6a"
+PKG_VERSION="684106fb3aa945412c51a4255ed23ce2ced190c9"
+PKG_SHA256="d5f7d1c391ef6f34bf074283dbbf57ee7021a7c0e2db0bda05f2dbc11de70e86"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/libretro-fceumm"
 PKG_URL="https://github.com/libretro/libretro-fceumm/archive/$PKG_VERSION.tar.gz"
