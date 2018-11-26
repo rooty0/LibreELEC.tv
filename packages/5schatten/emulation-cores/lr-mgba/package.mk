666bf6a5fd50a0ac49f7889476c528fd92a99d33
5schatten <supervisedthinking@gmail.com>
2018-11-26 17:50:15 +0100

    lr-mgba: switched to upstream repo / changed toolchain to cmake-make / updated to git 38bad71

--- packages/5schatten/emulation-cores/lr-mgba/package.mk
+++ packages/5schatten/emulation-cores/lr-mgba/package.mk
@@ -2,43 +2,33 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-mgba"
-PKG_VERSION="c32d83154010453409bf7b1f56e10404972ad127"
-PKG_SHA256="1c756b1f1950ddec9ca73e424ee92a09c57634153997e4e59a47df5d9cbd7968"
+PKG_VERSION="38bad71e119aa71f9c89d066fdfe589d3cdfd2e6"
+PKG_SHA256="851ef73c10e28caa5d3569c50cdd8cfe4a5ce304ea0c5e0699259669ae3bebd5"
 PKG_LICENSE="MPL 2.0"
-PKG_SITE="https://github.com/libretro/mgba"
-PKG_URL="https://github.com/libretro/mgba/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/mgba-emu/mgba"
+PKG_URL="https://github.com/mgba-emu/mgba/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain retroarch"
-PKG_LONGDESC="mGBA is an emulator for running Game Boy Advance games. It aims to be faster and more accurate than many existing Game Boy Advance emulators, as well as adding features that other emulators lack."
-PKG_TOOLCHAIN="manual"
+PKG_LONGDESC="mGBA is an emulator for running Game Boy Advance games."
+PKG_TOOLCHAIN="cmake-make"
 
 PKG_LIBNAME="mgba_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
 
-pre_configure_target() {
-  # fails to build in subdirs
-  cd $PKG_BUILD
-  rm -rf .$TARGET_NAME
-}
+if [ "$OPENGL_SUPPORT" = "yes" ] && [ "$PROJECT" = "Generic" ]; then
+  PKG_DEPENDS_TARGET+=" libepoxy"
 
-make_target() {
-  if [ "$PROJECT" == "Amlogic" ]; then
-      make -f Makefile.libretro platform=unix-armv HAVE_NEON=1 GIT_VERSION=${PKG_VERSION:0:7}
-  fi
+elif [ "$OPENGLES_SUPPORT" = "yes" ]; then
+  MGBA_SUPPORT_OPENGLES="-DBUILD_GLES2=ON"
+fi
 
-  if [ "$PROJECT" == "Generic" ]; then
-      make -f Makefile.libretro GIT_VERSION=${PKG_VERSION:0:7}
-  fi
-
-  if [ "$PROJECT" == "RPi" ]; then
-    case $DEVICE in
-      RPi)
-      make -f Makefile.libretro platform=unix-armv HAVE_NEON=1 GIT_VERSION=${PKG_VERSION:0:7}
-        ;;
-      RPi2)
-      make -f Makefile.libretro platform=unix-armv CC=$CC CXX=$CXX GIT_VERSION=${PKG_VERSION:0:7}
-        ;;
-    esac
-  fi
+PKG_CMAKE_OPTS_TARGET="-DUSE_DEBUGGERS=OFF \
+                       -DBUILD_QT=OFF \
+                       -DBUILD_SDL=OFF \
+                       -DBUILD_LIBRETRO=ON \
+                       -DSKIP_LIBRARY=ON \
+                       $MGBA_SUPPORT_OPENGLES"
+pre_make_target() {
+  sed -i -e "s/set(VERSION_STRING \${GIT_BRANCH}-\${GIT_REV}-\${GIT_COMMIT_SHORT})/set(VERSION_STRING master-${PKG_VERSION:0:7})/" $PKG_BUILD/version.cmake
 }
 
 makeinstall_target() {
