df9662b4ad42f980af7b9a9d233baed1a8423a92
5schatten <supervisedthinking@gmail.com>
2018-11-27 14:02:44 +0100

    lr-mrboom: updated to git dec629b / disable NEON to fix ARM builds

--- packages/5schatten/emulation-cores/lr-mrboom/package.mk
+++ packages/5schatten/emulation-cores/lr-mrboom/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-mrboom"
-PKG_VERSION="2865840b64ef97b7288ce4514effa0790ca7acbd" # v4.5
-PKG_SHA256="d9ed48bdeb9aa521a8e3e0257fdf6c9e77eec68bcdc00ddbfe418346a3c14d6d"
+PKG_VERSION="dec629bcb9dabef2e15aceb9a0fca9bc67be7862" # v4.5+
+PKG_SHA256="0e10b3a16f47e58410946992e88ca78b77a367f0d75d9c307769fc34a5364d2e"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/mrboom-libretro"
 PKG_URL="https://github.com/libretro/mrboom-libretro/archive/$PKG_VERSION.tar.gz"
@@ -14,6 +14,12 @@ PKG_LIBNAME="mrboom_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
 
 make_target() {
+  # Disable NEON otherwise build fails
+  if target_has_feature neon; then
+    CFLAGS="$CFLAGS -DDONT_WANT_ARM_OPTIMIZATIONS" 
+  fi
+
+  make clean
   make GIT_VERSION=${PKG_VERSION:0:7}
 }
 
