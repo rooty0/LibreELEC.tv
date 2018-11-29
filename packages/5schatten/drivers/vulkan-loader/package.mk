4a0f618dbf99f7e4704163ad08a5d2867bee1249
5schatten <supervisedthinking@gmail.com>
2018-11-29 11:57:02 +0100

    vulkan-loader: update to 1.1.94

--- packages/5schatten/drivers/vulkan-loader/package.mk
+++ packages/5schatten/drivers/vulkan-loader/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="abe5c2b3c33e42fff883d32d4c168556fb29b3ba" # 1.1.92
-PKG_SHA256="e3c6fa611a3e91e1f35d760aa827e392a73b0d132d037059a1e0e7b550e68f33"
+PKG_VERSION="5cdde103accaecaf6cc4a67243dd504da48b95f7" # 1.1.94
+PKG_SHA256="e8bf69c50285e57095c523fd4b345742c4555ffb15ce2e9fe2cc2637b2fd5b0d"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/$PKG_VERSION.tar.gz"
