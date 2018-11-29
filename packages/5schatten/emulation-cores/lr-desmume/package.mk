751e29e4bdb70191cb90c67239481959b4c4f563
5schatten <supervisedthinking@gmail.com>
2018-11-30 00:56:31 +0100

    lr-desmume: updated make toolchain

--- packages/5schatten/emulation-cores/lr-desmume/package.mk
+++ packages/5schatten/emulation-cores/lr-desmume/package.mk
@@ -9,26 +9,25 @@ PKG_SITE="https://github.com/libretro/desmume"
 PKG_URL="https://github.com/libretro/desmume/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain retroarch"
 PKG_LONGDESC="DeSmuME is a Nintendo DS emulator"
-PKG_TOOLCHAIN="manual"
+PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="desmume_libretro.so"
 PKG_LIBPATH="desmume/src/frontend/libretro/$PKG_LIBNAME"
 
 # Disable OpenGL if not supported
-if [ $OPENGL_SUPPORT = "no" ]; then
+if [ ! $OPENGL_SUPPORT = "yes" ]; then
   PKG_PATCH_DIRS="no-opengl"
 fi
 
-make_target() {
+PKG_MAKE_OPTS_TARGET="-C desmume/src/frontend/libretro -f Makefile.libretro GIT_VERSION=${PKG_VERSION:0:7}"
+
+pre_make_target() {
   case $TARGET_CPU in
     arm1176jzf-s)
-      make -C desmume/src/frontend/libretro -f Makefile.libretro platform=armv6-hardfloat-$TARGET_CPU GIT_VERSION=${PKG_VERSION:0:7}
+      PKG_MAKE_OPTS_TARGET+=" platform=armv6-hardfloat-$TARGET_CPU"
       ;;
     cortex-a7|cortex-a9|cortex-a53)
-      make -C desmume/src/frontend/libretro -f Makefile.libretro platform=armv7-neon-hardfloat-$TARGET_CPU GIT_VERSION=${PKG_VERSION:0:7}
-      ;;
-    x86-64)
-      make -C desmume/src/frontend/libretro -f Makefile.libretro GIT_VERSION=${PKG_VERSION:0:7}
+      PKG_MAKE_OPTS_TARGET+=" platform=armv7-neon-hardfloat-$TARGET_CPU"
       ;;
   esac
 }
