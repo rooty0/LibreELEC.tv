f9689d8e3f5004f1b5262586c32e69a7df22fe9e
5schatten <supervisedthinking@gmail.com>
2018-11-30 00:57:42 +0100

    lr-reicast: updated make toolchain / updated to git bd64deb

--- packages/5schatten/emulation-cores/lr-reicast/package.mk
+++ packages/5schatten/emulation-cores/lr-reicast/package.mk
@@ -2,40 +2,42 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-reicast"
-PKG_VERSION="f029cf92488f7f4ac8b94f93cc3b3f648420b643"
-PKG_SHA256="777d7b5cd91828b3ced9d1b11cf253d6ec4bca70f6fc7343e43a2ae243616710"
+PKG_VERSION="bd64deb3fe05b6b1a85ae75518dee1de18134649"
+PKG_SHA256="9551c2e1a4729c3bf5d2e960924b25b04f049b17011789204b7025270ea7e03d"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/reicast-emulator"
 PKG_URL="https://github.com/libretro/reicast-emulator/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain retroarch"
 PKG_LONGDESC="Reicast is a multiplatform Sega Dreamcast emulator"
-PKG_TOOLCHAIN="manual"
+PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="-gold"
 
 PKG_LIBNAME="reicast_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
 
+PKG_MAKE_OPTS_TARGET="HAVE_OPENMP=0 GIT_VERSION=${PKG_VERSION:0:7} WITH_DYNAREC=$ARCH"
+
 pre_make_target() {
- export BUILD_SYSROOT=$SYSROOT_PREFIX
-}
+  export BUILD_SYSROOT=$SYSROOT_PREFIX
 
-make_target() {
-  if [ "$PROJECT" = "Amlogic" ]; then
-    make FORCE_GLES=1 HAVE_OPENMP=0 GIT_VERSION=${PKG_VERSION:0:7} WITH_DYNAREC=$ARCH platform=rpi3
-  
-  elif [ "$PROJECT" = "RPi" ]; then
-    case $DEVICE in
-      RPi)
-        make FORCE_GLES=1 HAVE_OPENMP=0 GIT_VERSION=${PKG_VERSION:0:7} WITH_DYNAREC=$ARCH platform=rpi
-        ;;
-      RPi2)
-        make FORCE_GLES=1 HAVE_OPENMP=0 GIT_VERSION=${PKG_VERSION:0:7} WITH_DYNAREC=$ARCH platform=rpi2
-        ;;
-    esac
-
-  else
-    make AS=${AS} CC_AS=${AS} HAVE_OPENMP=0 HAVE_OIT=1 GIT_VERSION=${PKG_VERSION:0:7} WITH_DYNAREC=$ARCH
+  if [ "$OPENGLES_SUPPORT" = "yes" ]; then
+    PKG_MAKE_OPTS_TARGET+=" FORCE_GLES=1"
   fi
+
+  case $PROJECT in
+    Amlogic)
+      PKG_MAKE_OPTS_TARGET+=" platform=rpi3"
+      ;;
+    RPi)
+      PKG_MAKE_OPTS_TARGET+=" platform=rpi"
+      ;;
+    RPi2)
+      PKG_MAKE_OPTS_TARGET+=" platform=rpi2"
+      ;;
+    Generic)
+      PKG_MAKE_OPTS_TARGET+=" AS=${AS} CC_AS=${AS} HAVE_OIT=1"
+      ;;
+  esac
 }
 
 makeinstall_target() {
