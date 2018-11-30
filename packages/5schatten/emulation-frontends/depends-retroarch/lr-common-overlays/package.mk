930b9b313a9930888c610390155d6d68dd2347d3
5schatten <supervisedthinking@gmail.com>
2018-11-30 16:55:18 +0100

    lr-common-overlays: renamed retroarch-common-overlays to lr-common-overlays

--- packages/5schatten/emulation-frontends/depends-retroarch/lr-common-overlays/package.mk
+++ packages/5schatten/emulation-frontends/depends-retroarch/lr-common-overlays/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
-PKG_NAME="retroarch-common-overlays"
+PKG_NAME="lr-common-overlays"
 PKG_VERSION="8570b844235eadfdee8b4761f6912bd97d7b53e0"
 PKG_SHA256="8d10103de2ed4a9e1a94f28a77eec01734a181f719ec1f739e8c7b08fcceee52"
 PKG_LICENSE="GPL"
