9f122403a77900b5a7f6f0fa706d458faee56c96
5schatten <supervisedthinking@gmail.com>
2018-12-01 23:39:03 +0100

    amiberry: updated to git e0250cf / v2.24b10

--- packages/5schatten/emulation-standalone/amiberry/package.mk
+++ packages/5schatten/emulation-standalone/amiberry/package.mk
@@ -2,27 +2,24 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="amiberry"
-PKG_VERSION="c4cf0a009c4efabc0bbcc1527c5afd9ec1eafb3d" # v2.24b9
+PKG_VERSION="e0250cf4eacbb60b52ce90776ebe296ea6e82cd2" # v2.24b10
 PKG_ARCH="arm"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/midwan/amiberry"
 PKG_URL="https://github.com/midwan/amiberry.git"
 PKG_DEPENDS_TARGET="toolchain zlib SDL2-git SDL2_image SDL2_ttf capsimg libxml2 flac mpg123-compat libpng libmpeg2 retroarch-joypad-autoconfig"
-PKG_LONGDESC="Amiberry is an optimized Amiga emulator, for ARM-based boards (like the Raspberry Pi, ASUS Tinkerboard, Odroid XU4, etc). The core emulation comes from WinUAE, but stripped down to remove excess stuff in order to achieve good performance in underpowered boards. It includes ARM JIT support, to get high-performance results on CPU-intensive emulated environments."
+PKG_LONGDESC="Amiberry is an optimized Amiga emulator, for ARM-based boards (like the Raspberry Pi, ASUS Tinkerboard, Odroid XU4, etc)."
 GET_HANDLER_SUPPORT="git"
 PKG_TOOLCHAIN="manual"
 
 make_target() {
-
-  # build amiberry
   export SYSROOT_PREFIX=$SYSROOT_PREFIX
 
-  if [ "$PROJECT" = "Amlogic" ]; then
+  # build amiberry
+  if [ "${PROJECT}" = "Amlogic" ]; then
     make all PLATFORM=aml
-  fi
-
-  if [ "$PROJECT" = "RPi" ]; then
-    case $DEVICE in
+  elif [ "${PROJECT}" = "RPi" ]; then
+    case ${DEVICE} in
       RPi)
         make all PLATFORM=rpi1-sdl2
         ;;
@@ -35,13 +32,13 @@ make_target() {
 
 makeinstall_target() {
 
-  # create directories
+  # Create directories
   mkdir -p $INSTALL/usr/bin
   mkdir -p $INSTALL/usr/lib
   mkdir -p $INSTALL/usr/config/amiberry
   mkdir -p $INSTALL/usr/config/amiberry/controller
 
-  # copy ressources
+  # Copy ressources
   cp -R $PKG_DIR/config/* $INSTALL/usr/config/amiberry
   cp -R data $INSTALL/usr/config/amiberry/
   ln -s /storage/roms/bios/Kickstarts $INSTALL/usr/config/amiberry/kickstarts
@@ -49,11 +46,11 @@ makeinstall_target() {
   cp -R screenshots $INSTALL/usr/config/amiberry/
   cp -R whdboot $INSTALL/usr/config/amiberry/
 
-  # create links to Retroarch controller files
+  # Create links to Retroarch controller files
   ln -s /usr/share/retroarch/autoconfig/udev/8Bitdo_Pro_SF30_BT_B.cfg "$INSTALL/usr/config/amiberry/controller/8Bitdo SF30 Pro.cfg"
   ln -s "/usr/share/retroarch/autoconfig/udev/Pro Controller.cfg" "$INSTALL/usr/config/amiberry/controller/Pro Controller.cfg"
 
-  # copy binary, scripts & capsimg lib
+  # Copy binary, scripts & link libcapsimg
   cp amiberry-*-sdl2 $INSTALL/usr/bin/amiberry
   cp $PKG_DIR/scripts/* $INSTALL/usr/bin
 
