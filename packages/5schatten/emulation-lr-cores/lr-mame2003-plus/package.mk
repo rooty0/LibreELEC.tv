bbd7aec959caf1f960b80653db06b2912795fa10
5schatten <supervisedthinking@gmail.com>
2018-12-03 21:45:05 +0100

    lr-mame2003-plus: updated to git fc627d9 / switched PKG_TOOLCHAIN to "make"

--- packages/5schatten/emulation-lr-cores/lr-mame2003-plus/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mame2003-plus/package.mk
@@ -3,27 +3,24 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-mame2003-plus"
-PKG_VERSION="357d098e028483b612ce218229b250cef8c04511"
-PKG_SHA256="d875cb7e10cd4260509dc6b05a3ff52ff038bbbf8da4a1c122b1b7bd5739e480"
+PKG_VERSION="fc627d98c617672292a5ba63dd9705e453915a1b"
+PKG_SHA256="bb7199081d532b6efdc5f888a1f483755bf46020e8be6fa03c7e9e948da00b52"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame2003-plus-libretro"
 PKG_URL="https://github.com/libretro/mame2003-plus-libretro/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain retroarch"
 PKG_LONGDESC="Updated 2018 version of MAME (0.78) for libretro. with added game support plus many fixes and improvements"
+PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="mame2003_plus_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
 
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
 configure_target() {
-  export LD="$CC"
-}
-
-pre_make_target() {
+  # Set skip Disclaimer to enabled
   sed -e "s/\"Skip Disclaimer; disabled|enabled\"/\"Skip Disclaimer; enabled|disabled\"/" -i ${PKG_BUILD}/src/mame2003/mame2003.c
-}
-
-make_target() {
-  make GIT_VERSION=${PKG_VERSION:0:7}
+  export LD="$CC"
 }
 
 makeinstall_target() {
