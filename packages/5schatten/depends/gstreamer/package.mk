08f2601f7b8cea0bf2ac78000200f90ea590cbca
5schatten <supervisedthinking@gmail.com>
2018-12-06 19:05:31 +0100

    gstreamer: initial package v1.14.4

---
+++ packages/5schatten/depends/gstreamer/package.mk
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="gstreamer"
+PKG_VERSION="1.14.4"
+PKG_SHA256="f94f6696c5f05a3b3a9183e39c5f5c0b779f75a04c0efa497e7920afa985ffc7"
+PKG_LICENSE="GPL"
+PKG_SITE="https://gstreamer.freedesktop.org"
+PKG_URL="https://gstreamer.freedesktop.org/src/gstreamer/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="GStreamer open-source multimedia framework core library"
+PKG_TOOLCHAIN="configure"
+
+PKG_CONFIGURE_OPTS_TARGET="--with-unwind=no"
+
+post_makeinstall_target(){
+  # Clean up
+  rm -rf $INSTALL/usr/bin
+  rm -rf $INSTALL/usr/share
+}
+
