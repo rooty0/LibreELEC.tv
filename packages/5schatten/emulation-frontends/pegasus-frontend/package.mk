afd69c6c03e98e907ba28a7341346334a0454591
5schatten <supervisedthinking@gmail.com>
2018-12-06 21:02:16 +0100

    pegasus-frontend: initial package git 6e0bdef

---
+++ packages/5schatten/emulation-frontends/pegasus-frontend/package.mk
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="pegasus-frontend"
+PKG_VERSION="6e0bdeffd3f148f60429c2739db5628a535621dc" # Alpha 10+
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/mmatyas/pegasus-frontend"
+PKG_URL="https://github.com/mmatyas/pegasus-frontend.git"
+PKG_DEPENDS_TARGET="toolchain SDL2-git qt-everywhere pegasus-theme-es2-simple pegasus-theme-gameOS"
+PKG_LONGDESC="A cross platform, customizable graphical frontend for launching emulators and managing your game collection."
+GET_HANDLER_SUPPORT="git"
+PKG_TOOLCHAIN="manual"
+
+post_unpack() {
+  cp -r ${PKG_DIR}/files/logos/* ${PKG_BUILD}/src/themes/pegasus-theme-grid/assets/logos
+}
+
+make_target() {
+  cd $PKG_BUILD
+  mkdir -p .${TARGET_NAME}
+  cd .${TARGET_NAME}
+  qmake $PKG_BUILD/pegasus.pro INSTALLDIR=${INSTALL}/usr/bin \
+                               INSTALL_BINDIR=${INSTALL}/usr/bin \
+                               INSTALL_DATADIR=${INSTALL}/usr/bin \
+                               INSTALL_ICONDIR=${INSTALL}/usr/bin \
+                               INSTALL_DESKTOPDIR=${INSTALL}/usr/bin
+  make
+  make install
+}
+
+post_makeinstall_target() {
+  # Install start scripts
+  mkdir -p $INSTALL/usr/bin
+  cp $PKG_DIR/scripts/pegasus-fe-${PROJECT}.start $INSTALL/usr/bin/pegasus-fe.start
+
+  # Clean up
+  rm -rf $INSTALL/usr/bin/pegasus-fe.desktop
+  rm -rf $INSTALL/usr/bin/pegasus-fe.png
+}
