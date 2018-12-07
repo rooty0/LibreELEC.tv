c6fe2ebca34bb384cf35968efc2faf7dcbd18b03
5schatten <supervisedthinking@gmail.com>
2018-12-07 16:26:31 +0100

    qt-everywhere: added OpenGLES flags for Amlogic & RPi

--- packages/5schatten/depends/qt-everywhere/package.mk
+++ packages/5schatten/depends/qt-everywhere/package.mk
@@ -98,28 +98,38 @@ configure_target() {
 
   cd ..
   mkdir -p $QMAKE_CONF_DIR
-  echo "MAKEFILE_GENERATOR       = UNIX" > $QMAKE_CONF
-  echo "CONFIG                  += incremental" >> $QMAKE_CONF
-  echo "QMAKE_INCREMENTAL_STYLE  = sublib" >> $QMAKE_CONF
-  echo "include(../../common/linux.conf)" >> $QMAKE_CONF
+
+  # Set QMake Platform Flags
+  echo "MAKEFILE_GENERATOR       = UNIX"           > $QMAKE_CONF
+  echo "CONFIG                  += incremental"   >> $QMAKE_CONF
+  echo "QMAKE_INCREMENTAL_STYLE  = sublib"        >> $QMAKE_CONF
+  echo "include(../../common/linux.conf)"         >> $QMAKE_CONF
   echo "include(../../common/gcc-base-unix.conf)" >> $QMAKE_CONF
-  echo "include(../../common/g++-unix.conf)" >> $QMAKE_CONF
-  echo "load(device_config)" >> $QMAKE_CONF
-  echo "QMAKE_CC                = $CC" >> $QMAKE_CONF
-  echo "QMAKE_CXX               = $CXX" >> $QMAKE_CONF
-  echo "QMAKE_LINK              = $CXX" >> $QMAKE_CONF
-  echo "QMAKE_LINK_SHLIB        = $CXX" >> $QMAKE_CONF
-  echo "QMAKE_AR                = $AR cqs" >> $QMAKE_CONF
-  echo "QMAKE_OBJCOPY           = $OBJCOPY" >> $QMAKE_CONF
-  echo "QMAKE_NM                = $NM -P" >> $QMAKE_CONF
-  echo "QMAKE_STRIP             = $STRIP" >> $QMAKE_CONF
-  echo "QMAKE_CFLAGS = $CFLAGS" >> $QMAKE_CONF
-  echo "QMAKE_CXXFLAGS = $CXXFLAGS" >> $QMAKE_CONF
-  echo "QMAKE_LFLAGS = $LDFLAGS" >> $QMAKE_CONF
-  echo "load(qt_config)" >> $QMAKE_CONF
+  echo "include(../../common/g++-unix.conf)"      >> $QMAKE_CONF
+  echo "load(device_config)"                      >> $QMAKE_CONF
+  echo "QMAKE_CC                = $CC"            >> $QMAKE_CONF
+  echo "QMAKE_CXX               = $CXX"           >> $QMAKE_CONF
+  echo "QMAKE_LINK              = $CXX"           >> $QMAKE_CONF
+  echo "QMAKE_LINK_SHLIB        = $CXX"           >> $QMAKE_CONF
+  echo "QMAKE_AR                = $AR cqs"        >> $QMAKE_CONF
+  echo "QMAKE_OBJCOPY           = $OBJCOPY"       >> $QMAKE_CONF
+  echo "QMAKE_NM                = $NM -P"         >> $QMAKE_CONF
+  echo "QMAKE_STRIP             = $STRIP"         >> $QMAKE_CONF
+  echo "QMAKE_CFLAGS = $CFLAGS"                   >> $QMAKE_CONF
+  echo "QMAKE_CXXFLAGS = $CXXFLAGS"               >> $QMAKE_CONF
+  echo "QMAKE_LFLAGS = $LDFLAGS"                  >> $QMAKE_CONF
+  if [ "${OPENGLES}" = "opengl-meson" ]; then
+    echo "QMAKE_LIBS_EGL += -lMali"               >> $QMAKE_CONF
+    echo "EGLFS_DEVICE_INTEGRATION = eglfs_mali"  >> $QMAKE_CONF
+  elif [ "${OPENGLES}" = "bcm2835-driver" ]; then
+    echo "QMAKE_LIBS_EGL += -lEGL -lGLESv2"       >> $QMAKE_CONF
+    echo "EGLFS_DEVICE_INTEGRATION = eglfs_brcm"  >> $QMAKE_CONF
+  fi
+  echo "load(qt_config)"                          >> $QMAKE_CONF
   echo '#include "../../linux-g++/qplatformdefs.h"' >> $QMAKE_CONF_DIR/qplatformdefs.h
 
   unset CC CXX LD RANLIB AR AS CPPFLAGS CFLAGS LDFLAGS CXXFLAGS
+  export QT_FORCE_PKGCONFIG=yes
   ./configure $PKG_CONFIGURE_OPTS_TARGET
 }
 
@@ -142,9 +152,15 @@ post_makeinstall_target() {
   cp -PR $SYSROOT_PREFIX/usr/lib/libQt5OpenGL.so*             $INSTALL/usr/lib
   cp -PR $SYSROOT_PREFIX/usr/lib/libQt5Qml.so*                $INSTALL/usr/lib
   cp -PR $SYSROOT_PREFIX/usr/lib/libQt5Quick.so*              $INSTALL/usr/lib
+  cp -PR $SYSROOT_PREFIX/usr/lib/libQt5QuickControls2.so*     $INSTALL/usr/lib
+  cp -PR $SYSROOT_PREFIX/usr/lib/libQt5QuickParticles.so*     $INSTALL/usr/lib
+  cp -PR $SYSROOT_PREFIX/usr/lib/libQt5QuickTemplates2.so*    $INSTALL/usr/lib
+  cp -PR $SYSROOT_PREFIX/usr/lib/libQt5QuickTest.so*          $INSTALL/usr/lib
   cp -PR $SYSROOT_PREFIX/usr/lib/libQt5Sql.so*                $INSTALL/usr/lib
   cp -PR $SYSROOT_PREFIX/usr/lib/libQt5Svg.so*                $INSTALL/usr/lib
+  cp -PR $SYSROOT_PREFIX/usr/lib/libQt5Test.so*               $INSTALL/usr/lib
   cp -PR $SYSROOT_PREFIX/usr/lib/libQt5Widgets.so*            $INSTALL/usr/lib
+
 
   # Install Qt5 plugins
   cp -PR $SYSROOT_PREFIX/usr/plugins/audio             $INSTALL/usr/plugins
