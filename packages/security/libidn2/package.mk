cf8d06c16d726d3a0baec5419696f46bc7498377
CvH <namerp@googlemail.com>
2018-12-12 23:59:48 +0100

    libidn2: initial package

---
+++ packages/security/libidn2/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="libidn2"
+PKG_VERSION="2.0.5"
+PKG_SHA256="53f69170886f1fa6fa5b332439c7a77a7d22626a82ef17e2c1224858bb4ca2b8"
+PKG_LICENSE="LGPL3"
+PKG_SITE="https://www.gnu.org/software/libidn/"
+PKG_URL="http://ftpmirror.gnu.org/gnu/libidn/libidn2-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Free software implementation of IDNA2008, Punycode and TR46."
