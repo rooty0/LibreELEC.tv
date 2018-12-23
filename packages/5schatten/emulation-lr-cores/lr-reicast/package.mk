658019eebe052960a4aeef966065619cdd34330d
5schatten <supervisedthinking@gmail.com>
2018-12-23 18:56:42 +0100

    lr-reicast: updated to git 3dd45d2 / dropped patches

--- packages/5schatten/emulation-lr-cores/lr-reicast/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-reicast/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-reicast"
-PKG_VERSION="37da602d6421b755ae045171d17c8d4328c7dcc0"
-PKG_SHA256="a66c85dd3af8b3babbc8381529827cbe82ea92fced02a5f84d4587d9377ade25"
+PKG_VERSION="3dd45d27159278e88a73352a318d0b23bc241a1a"
+PKG_SHA256="7a7b019d59cc70b0586bdb3c8eceddb1604e099d5927fdd7969f0fdb66302a5d"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/reicast-emulator"
 PKG_URL="https://github.com/libretro/reicast-emulator/archive/$PKG_VERSION.tar.gz"
