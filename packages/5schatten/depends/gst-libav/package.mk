a0b1eb7a7b07562dcd38bffb114d51c11e167b5f
5schatten <supervisedthinking@gmail.com>
2018-12-23 15:17:53 +0100

    gst-libav: switched to freedesktop repo / use configure toolchain / updated FFmpeg 4 patch

--- packages/5schatten/depends/gst-libav/package.mk
+++ packages/5schatten/depends/gst-libav/package.mk
@@ -3,15 +3,10 @@
 
 PKG_NAME="gst-libav"
 PKG_VERSION="1.14.4"
-PKG_SHA256="7c1749e4c43082607ee3d4e80b4efd2f29ebc03667491d237840d5a693e4718f"
+PKG_SHA256="dfd78591901df7853eab7e56a86c34a1b03635da0d3d56b89aa577f1897865da"
 PKG_LICENSE="GPL"
 PKG_SITE="https://gstreamer.freedesktop.org/modules/gst-libav.html"
-PKG_URL="https://github.com/GStreamer/gst-libav/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://gstreamer.freedesktop.org/src/gst-libav/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain gstreamer ffmpeg"
 PKG_LONGDESC="GStreamer plugin for the libav* library (former FFmpeg)"
-
-makeinstall_target(){
-  # Install lib
-  mkdir -p $INSTALL/usr/lib
-  cp -PR ext/libav/libgstlibav.so $INSTALL/usr/lib
-}
+PKG_TOOLCHAIN="configure"
