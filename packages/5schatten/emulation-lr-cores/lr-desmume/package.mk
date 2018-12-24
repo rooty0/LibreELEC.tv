11027733345e79203bb0d7b2947567d62d4c9ebd
5schatten <supervisedthinking@gmail.com>
2018-12-24 19:04:51 +0100

    lr-desmume: updated to git fe58f1e

--- packages/5schatten/emulation-lr-cores/lr-desmume/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-desmume/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-desmume"
-PKG_VERSION="1ac359d8f92faf911386910e74c76aa6f5e9722e"
-PKG_SHA256="d28285adfa135278270f22f7ae25a8cad83fbc8d4541e24f609a5c51ae7dc438"
+PKG_VERSION="fe58f1e201f998fcdb9bd333ab431ded81c52e4c"
+PKG_SHA256="c8e24a2f82f7b18cd7cf514db3f9b1e2998f768dbfdf3a39a7f0235af2b41e9a"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/desmume"
 PKG_URL="https://github.com/libretro/desmume/archive/$PKG_VERSION.tar.gz"
