d0e0af1872a72337ca8a975f992b8c3df3ddc89f
5schatten <supervisedthinking@gmail.com>
2018-12-30 00:16:12 +0100

    lr-database: updated to git 093d8c3

--- packages/5schatten/emulation-frontends/depends-retroarch/lr-database/package.mk
+++ packages/5schatten/emulation-frontends/depends-retroarch/lr-database/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-database"
-PKG_VERSION="25d808a8b27f9bd7bf9373b29fcd2b9858eb5b3d"
-PKG_SHA256="a5f594d3c8e334e8d53431bd1c527f2dcef7bea62c03a4616c178f74128dfc22"
+PKG_VERSION="093d8c37ebe3606e000225201d7f64baf657558a"
+PKG_SHA256="712fee68e0ab3ed3c3ac69d55236d2bb1a10896d312324f24b41fb1781799d7c"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-database"
 PKG_URL="https://github.com/libretro/libretro-database/archive/$PKG_VERSION.tar.gz"
