5d36cd856b154d3e7d440b09c6526968d92333ec
5schatten <supervisedthinking@gmail.com>
2018-12-31 15:34:15 +0100

    lr-mgba: updated to git 3fb46a2 / enable ninja build

--- packages/5schatten/emulation-lr-cores/lr-mgba/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mgba/package.mk
@@ -2,14 +2,13 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-mgba"
-PKG_VERSION="6442d17b4c0012a68683b052a0c1bbafd2318722"
-PKG_SHA256="22e54786b966b12c80a1f872b268fe804205d3424afd19b56f7092ea4552ed4b"
+PKG_VERSION="3fb46a2a8832c383429fd65c00ba3e642c750bcf"
+PKG_SHA256="8170a9aab40329aa2bdfc43857f6f798a5549283b04d6f68e1795c2065fd6a7a"
 PKG_LICENSE="MPL 2.0"
 PKG_SITE="https://github.com/mgba-emu/mgba"
 PKG_URL="https://github.com/mgba-emu/mgba/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain retroarch zlib"
 PKG_LONGDESC="mGBA is an emulator for running Game Boy Advance games."
-PKG_TOOLCHAIN="cmake-make"
 
 PKG_LIBNAME="mgba_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
