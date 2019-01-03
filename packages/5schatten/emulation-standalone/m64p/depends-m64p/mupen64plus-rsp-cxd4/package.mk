ee3d18151e64a42f9f3977bb092c337ea9e7eb7e
5schatten <supervisedthinking@gmail.com>
2019-01-03 04:13:09 +0100

    mupen64plus-rsp-cxd4: initial package git 6224403

---
+++ packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-rsp-cxd4/package.mk
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="mupen64plus-rsp-cxd4"
+PKG_VERSION="62244037ce5c04451760a5195da30823995196b9"
+PKG_SHA256="9ade4834f633eb2f5c6554620f6596b9c681057eeaf7798f3d84c257c1b69b07"
+PKG_LICENSE="CC0-1.0"
+PKG_SITE="https://github.com/mupen64plus/mupen64plus-rsp-cxd4"
+PKG_URL="https://github.com/mupen64plus/mupen64plus-rsp-cxd4/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain SDL2-git mupen64plus-core"
+PKG_LONGDESC="Exemplary MSP communications simulator using a normalized VU."
+PKG_TOOLCHAIN="manual"
+
+make_target() {
+  PKG_MAKE_OPTS_TARGET="all HLEVIDEO=1 APIDIR=$(get_build_dir mupen64plus-core)/src/api"
+
+  # ARM NEON optimization
+  if target_has_feature neon; then
+    PKG_MAKE_OPTS_TARGET+=" NEON=1"
+  fi
+
+  cd $PKG_BUILD/projects/unix
+  make -j$CONCURRENCY_MAKE_LEVEL ${PKG_MAKE_OPTS_TARGET}
+}
