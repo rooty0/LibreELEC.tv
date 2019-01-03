2bc718837bf8df2f03b998c0213d6821d9fac180
5schatten <supervisedthinking@gmail.com>
2019-01-04 00:14:30 +0100

    mupen64plus-input-sdl: fix build for RPi

--- packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-input-sdl/package.mk
+++ packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-input-sdl/package.mk
@@ -9,13 +9,14 @@ PKG_SITE="https://github.com/mupen64plus/mupen64plus-input-sdl"
 PKG_URL="https://github.com/mupen64plus/mupen64plus-input-sdl/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain SDL2-git mupen64plus-core"
 PKG_LONGDESC="Input plugin for Mupen64Plus v2.0 project using SDL. This is derived from the original Mupen64 blight_input plugin."
-PKG_TOOLCHAIN="manual"
+PKG_TOOLCHAIN="make"
 
-configure_target() {
+PKG_MAKE_OPTS_TARGET="-f projects/unix/Makefile SRCDIR=src APIDIR=$(get_build_dir mupen64plus-core)/src/api all"
+
+pre_configure_target() {
   export SYSROOT_PREFIX=$SYSROOT_PREFIX
 }
 
-make_target() {
-  cd $PKG_BUILD/projects/unix
-  make -j$CONCURRENCY_MAKE_LEVEL all APIDIR=$(get_build_dir mupen64plus-core)/src/api
+makeinstall_target() {
+ :
 }
