a92ad684ef1f636471e1452b369776e94bcb250d
5schatten <supervisedthinking@gmail.com>
2019-01-04 00:16:56 +0100

    m64p: fix build for RPi / added mupen64plus-ui-console start scripts / updated mupen64plus lib paths

--- packages/5schatten/emulation-standalone/m64p/m64p/package.mk
+++ packages/5schatten/emulation-standalone/m64p/m64p/package.mk
@@ -5,20 +5,31 @@ PKG_NAME="m64p"
 PKG_VERSION="1.0"
 PKG_SITE="https://m64p.github.io/"
 PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain mupen64plus-core mupen64plus-rsp-hle mupen64plus-rsp-cxd4 mupen64plus-input-sdl mupen64plus-audio-sdl2 mupen64plus-gui GLideN64 angrylion-rdp-plus"
+PKG_DEPENDS_TARGET="toolchain mupen64plus-core mupen64plus-rsp-hle mupen64plus-rsp-cxd4 mupen64plus-input-sdl mupen64plus-audio-sdl2 mupen64plus-ui-console GLideN64"
 PKG_LONGDESC="mupen64plus + GLideN64 + a GUI"
 PKG_TOOLCHAIN="manual"
+
+# Build angrylion-rdp-plus & mupen64plus-gui for Generic
+if [ "${PROJECT}" = "Generic" ]; then
+  PKG_DEPENDS_TARGET+=" mupen64plus-gui angrylion-rdp-plus"
+fi
 
 makeinstall_target() {
   # Create directories
   mkdir -p $INSTALL/usr/bin
   mkdir -p $INSTALL/usr/config/mupen64plus
-  mkdir -p $INSTALL/usr/lib
+  mkdir -p $INSTALL/usr/lib/mupen64plus
 
-  # Install binary & scripts
-  cp $PKG_DIR/scripts/* $INSTALL/usr/bin
-  cp $(get_build_dir mupen64plus-gui)/.${TARGET_NAME}/mupen64plus-gui $INSTALL/usr/bin
-  
+  if [ "${PROJECT}" = "Generic" ]; then
+    cp $(get_build_dir angrylion-rdp-plus)/.${TARGET_NAME}/*.so         $INSTALL/usr/lib/mupen64plus
+    cp $(get_build_dir mupen64plus-gui)/.${TARGET_NAME}/mupen64plus-gui $INSTALL/usr/bin
+    cp $PKG_DIR/scripts/gui/*                                           $INSTALL/usr/bin
+  fi
+
+  # Install binaries & scripts
+  cp $(get_build_dir mupen64plus-ui-console)/mupen64plus $INSTALL/usr/bin/mupen64plus-cli
+  cp $PKG_DIR/scripts/cli/m64p-cli-${PROJECT}.start      $INSTALL/usr/bin/m64p-cli.start
+
   # Install config files
   cp $PKG_DIR/config/*                             $INSTALL/usr/config/mupen64plus
   cp $(get_build_dir GLideN64)/ini/*.ini           $INSTALL/usr/config/mupen64plus
@@ -26,11 +37,10 @@ makeinstall_target() {
   cp $(get_build_dir mupen64plus-input-sdl)/data/* $INSTALL/usr/config/mupen64plus
 
   # Install libs
-  cp $(get_build_dir angrylion-rdp-plus)/.${TARGET_NAME}/*.so     $INSTALL/usr/lib
-  cp $(get_build_dir GLideN64)/projects/cmake/plugin/Release/*.so $INSTALL/usr/lib
-  cp $(get_build_dir mupen64plus-audio-sdl2)/projects/unix/*.so   $INSTALL/usr/lib
-  cp -P $(get_build_dir mupen64plus-core)/projects/unix/*.so*     $INSTALL/usr/lib
-  cp $(get_build_dir mupen64plus-input-sdl)/projects/unix/*.so    $INSTALL/usr/lib
-  cp $(get_build_dir mupen64plus-rsp-cxd4)/projects/unix/*.so     $INSTALL/usr/lib
-  cp $(get_build_dir mupen64plus-rsp-hle)/projects/unix/*.so      $INSTALL/usr/lib
+  cp $(get_build_dir GLideN64)/plugin/Release/*.so $INSTALL/usr/lib/mupen64plus
+  cp $(get_build_dir mupen64plus-audio-sdl2)/*.so  $INSTALL/usr/lib/mupen64plus
+  cp -P $(get_build_dir mupen64plus-core)/*.so*    $INSTALL/usr/lib/mupen64plus
+  cp $(get_build_dir mupen64plus-input-sdl)/*.so   $INSTALL/usr/lib/mupen64plus
+  cp $(get_build_dir mupen64plus-rsp-cxd4)/*.so    $INSTALL/usr/lib/mupen64plus
+  cp $(get_build_dir mupen64plus-rsp-hle)/*.so     $INSTALL/usr/lib/mupen64plus
 }
