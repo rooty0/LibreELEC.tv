70e35b1f794c3786fee7a1af01caea618f473753
5schatten <supervisedthinking@gmail.com>
2019-01-03 04:12:24 +0100

    mupen64plus-input-sdl: initial package git f5c3995

---
+++ packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-input-sdl/package.mk
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="mupen64plus-input-sdl"
+PKG_VERSION="f5c3995d198ff3de9fd1cde85c0113b0133ac5a8"
+PKG_SHA256="a626bd42e387abc4d49c98dfe4af940e381b47343f17b83a01ea152ae1761871"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/mupen64plus/mupen64plus-input-sdl"
+PKG_URL="https://github.com/mupen64plus/mupen64plus-input-sdl/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain SDL2-git mupen64plus-core"
+PKG_LONGDESC="Input plugin for Mupen64Plus v2.0 project using SDL. This is derived from the original Mupen64 blight_input plugin."
+PKG_TOOLCHAIN="manual"
+
+configure_target() {
+  export SYSROOT_PREFIX=$SYSROOT_PREFIX
+}
+
+make_target() {
+  cd $PKG_BUILD/projects/unix
+  make -j$CONCURRENCY_MAKE_LEVEL all APIDIR=$(get_build_dir mupen64plus-core)/src/api
+}
