8e67d2c5f02468f3790d1807a1b12b3524afcb5b
5schatten <supervisedthinking@gmail.com>
2019-01-03 03:24:06 +0100

    vice: updated to v3.3 / switched to sourceforge repo / cleaned up a bit

--- packages/5schatten/emulation-standalone/vice/package.mk
+++ packages/5schatten/emulation-standalone/vice/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="vice"
-PKG_VERSION="3.2"
-PKG_SHA256="17c9ece18dc100a5be26ad02ad74e7583e06458a87aabab720db30e176716aee"
+PKG_VERSION="3.3"
+PKG_SHA256="1a55b38cc988165b077808c07c52a779d181270b28c14b5c9abf4e569137431d"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://vice-emu.sourceforge.net/"
-PKG_URL="https://github.com/SupervisedThinking/vice/archive/v$PKG_VERSION.tar.gz"
+PKG_URL="https://sourceforge.net/projects/vice-emu/files/releases/vice-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2-git libpng giflib zlib libvorbis libogg lame"
 PKG_LONGDESC="VICE C64 emulator"
 PKG_TOOLCHAIN="configure"
@@ -17,30 +17,33 @@ pre_configure_target() {
   PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_sdl2_config=$SYSROOT_PREFIX/usr/bin/sdl2-config \
                              toolchain_check=no \
                            --prefix=/usr \
+                           --enable-silent-rules \
                            --disable-option-checking \
                            --disable-catweasel \
                            --enable-native-tools=$HOST_CC \
                            --enable-sdlui2 \
                            --disable-parsid \
                            --disable-hardsid \
-                           --without-oss \
-                           --without-pulse"
+                           --without-oss"
 
   export LIBS="-ludev"
   $PKG_BUILD/autogen.sh
 }
 
 post_makeinstall_target() {
-  # copy over scripts
+  # Copy start script
   cp $PKG_DIR/scripts/* $INSTALL/usr/bin
-  # rename the lib directory as LE has a lib64 symlink to lib
+
+  # Rename the lib directory as LE has a lib64 symlink to lib
   if [ -d $INSTALL/usr/lib64 ]; then
     mv $INSTALL/usr/lib64 $INSTALL/usr/lib
   fi
-  # copy over default config
+
+  # Copy default config
   mkdir -p $INSTALL/etc
   cp $PKG_DIR/config/sdl-vicerc $INSTALL/etc/
-  # remove binaries
+
+  # Remove binaries
   for bin in \
     c1541 \
     cartconv \
@@ -58,7 +61,8 @@ post_makeinstall_target() {
   do
     rm $INSTALL/usr/bin/$bin
   done
-  # remove data files
+
+  # Remove data files
   for dir in \
     C128 \
     C64DTV \
@@ -69,6 +73,6 @@ post_makeinstall_target() {
     SCPU64 \
     VIC20
   do
-    rm -rf $INSTALL/usr/lib/vice/$dir
+    rm -rf $INSTALL/usr/lib/vice/${dir}
   done
  }
