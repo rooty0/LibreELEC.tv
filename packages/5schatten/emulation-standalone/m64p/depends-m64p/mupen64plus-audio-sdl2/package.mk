b0d93b65fb2d27e46e965a290e6546df57abdce0
5schatten <supervisedthinking@gmail.com>
2019-01-04 00:13:41 +0100

    mupen64plus-audio-sdl2: fix build for RPi

--- packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-audio-sdl2/package.mk
+++ packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-audio-sdl2/package.mk
@@ -7,15 +7,16 @@ PKG_SHA256="adda693b4343d9305174520027fa0b33a2311a815df48bdf19a08f228d0e2a41"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/m64p/mupen64plus-audio-sdl2"
 PKG_URL="https://github.com/m64p/mupen64plus-audio-sdl2/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain mupen64plus-core SDL2-git"
+PKG_DEPENDS_TARGET="toolchain mupen64plus-core libsamplerate SDL2-git"
 PKG_LONGDESC="A low-level N64 video emulation plugin, based on the pixel-perfect angrylion RDP plugin with some improvements."
-PKG_TOOLCHAIN="manual"
+PKG_TOOLCHAIN="make"
 
-configure_target() {
+PKG_MAKE_OPTS_TARGET="-f projects/unix/Makefile SRCDIR=src APIDIR=$(get_build_dir mupen64plus-core)/src/api all"
+
+pre_configure_target() {
   export SYSROOT_PREFIX=$SYSROOT_PREFIX
 }
 
-make_target() {
-  cd $PKG_BUILD/projects/unix
-  make -j$CONCURRENCY_MAKE_LEVEL all APIDIR=$(get_build_dir mupen64plus-core)/src/api
+makeinstall_target() {
+ :
 }
