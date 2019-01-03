06e31ec53b2d3931cd282e27a63584d048a32e2e
5schatten <supervisedthinking@gmail.com>
2019-01-04 00:13:59 +0100

    mupen64plus-core: fix build for RPi

--- packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-core/package.mk
+++ packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-core/package.mk
@@ -7,31 +7,40 @@ PKG_SHA256="e2403e8915032e519dc6b94f46820b10e05830992d46806ca47172020c8246cc"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/mupen64plus/mupen64plus-core"
 PKG_URL="https://github.com/mupen64plus/mupen64plus-core/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain SDL2-git freetype libpng zlib glu"
+PKG_DEPENDS_TARGET="toolchain SDL2-git freetype libpng zlib"
 PKG_LONGDESC="Core module of the Mupen64Plus project"
-PKG_TOOLCHAIN="manual"
+PKG_TOOLCHAIN="make"
 
-# Add nasm dependency for mupen64plus-core
+# Generic depends on nasm & glu
 if [ "${PROJECT}" = "Generic" ]; then
-  PKG_DEPENDS_TARGET+=" nasm:host"
+  PKG_DEPENDS_TARGET+=" nasm:host glu"
 fi
 
-configure_target() {
+PKG_MAKE_OPTS_TARGET="-C projects/unix SRCDIR=../../src all SHAREDIR=/usr/config/mupen64plus"
+
+pre_configure_target() {
   export SYSROOT_PREFIX=$SYSROOT_PREFIX
-}
 
-make_target() {
-  cd $PKG_BUILD/projects/unix
-  PKG_MAKE_OPTS_TARGET="all SHAREDIR=/usr/config/mupen64plus"
+  # ARCH arm
+  if [ "${ARCH}" = "arm" ]; then
+    PKG_MAKE_OPTS_TARGET+=" DYNAREC=arm HOST_CPU=armv7"
 
-  # ARM NEON optimization
-  if target_has_feature neon; then
-    PKG_MAKE_OPTS_TARGET+=" NEON=1"
+    # ARM NEON optimization
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+=" NEON=1"
+    fi
   fi
+  
   # build against GLESv2 instead of OpenGL
   if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
     PKG_MAKE_OPTS_TARGET+=" USE_GLES=1"
+    # RPi OpenGLES Features Support
+    if [ "${OPENGLES}" = "bcm2835-driver" ]; then
+      PKG_MAKE_OPTS_TARGET+=" VC=1"
+    fi
   fi
+}
 
-  make -j$CONCURRENCY_MAKE_LEVEL ${PKG_MAKE_OPTS_TARGET} 
+makeinstall_target() {
+  mv $PKG_BUILD/projects/unix/*.so* $PKG_BUILD/
 }
