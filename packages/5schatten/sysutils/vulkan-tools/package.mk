5da0a315e79f627bed4e4ecded15875bcc06b9d0
5schatten <supervisedthinking@gmail.com>
2019-01-06 18:33:32 +0100

    vulkan-tools: updated to git 5190ca9 / fixed ninja build

--- packages/5schatten/sysutils/vulkan-tools/package.mk
+++ packages/5schatten/sysutils/vulkan-tools/package.mk
@@ -2,25 +2,26 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="vulkan-tools"
-PKG_VERSION="85bf42b02acb3f3287f9cb1a1a18d53b9da70a73" # 1.1.96+
-PKG_SHA256="351877011b5b06e84ee885f94646ef5b5a0b3cdfe655027b05a8c16f7ba00931"
+PKG_VERSION="5190ca9e4c8e6eae1bacbfc93eb6013a5ee1901e" # 1.1.96+
+PKG_SHA256="7f7dcbe6f69147186bde3f5a19611a9ca66d83701e91569bf5ab4d31966d6b00"
 PKG_ARCH="x86_64"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Tools"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-tools/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain cmake:host vulkan-loader"
 PKG_LONGDESC="This project provides Vulkan tools and utilities that can assist development by enabling developers to verify their applications correct use of the Vulkan API."
-PKG_TOOLCHAIN="cmake-make"
-
-PKG_CMAKE_OPTS_TARGET="-DBUILD_ICD=Off \
-                       -DINSTALL_ICD=Off"
 
 pre_configure_target() {
- sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i $PKG_BUILD/cube/CMakeLists.txt
- sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i $PKG_BUILD/vulkaninfo/CMakeLists.txt
+  PKG_CMAKE_OPTS_TARGET="-DBUILD_ICD=Off \
+                         -DINSTALL_ICD=Off"
+
+  # Disable Wayland WSI support
+  sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i $PKG_BUILD/cube/CMakeLists.txt
+  sed -e "s/Build Wayland WSI support\" ON/Build Wayland WSI support\" OFF/" -i $PKG_BUILD/vulkaninfo/CMakeLists.txt
 }
 
 pre_make_target() {
-  # Fix stdlib.h error
-  find . -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+  # Fix cross compiling
+  find $PKG_BUILD -name flags.make -exec sed -i "s:isystem :I:g" \{} \;
+  find $PKG_BUILD -name build.ninja -exec sed -i "s:isystem :I:g" \{} \;
 }
