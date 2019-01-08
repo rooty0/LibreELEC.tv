cd36d50484543ec267d8d4e34f9001bf64174849
5schatten <supervisedthinking@gmail.com>
2019-01-08 14:04:13 +0100

    retroarch: updated to git c2047c8

--- packages/5schatten/emulation-frontends/retroarch/package.mk
+++ packages/5schatten/emulation-frontends/retroarch/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="retroarch"
-PKG_VERSION="54a795bfd77497f71139aa8afebf4bcc230e4933" #1.7.6-dev 
+PKG_VERSION="c2047c8c04818715cf606249db630a3836619aa2" #1.7.6-dev 
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="https://github.com/libretro/RetroArch.git"
