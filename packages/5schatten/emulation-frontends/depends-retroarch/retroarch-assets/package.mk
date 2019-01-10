d0fb7fd8bdac20868cefcc240eec65349de63e16
5schatten <supervisedthinking@gmail.com>
2019-01-10 22:39:30 +0100

    retroarch: updated to git 3561a12

--- packages/5schatten/emulation-frontends/depends-retroarch/retroarch-assets/package.mk
+++ packages/5schatten/emulation-frontends/depends-retroarch/retroarch-assets/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="59e1783e2079f60c3532ab26c4766d48adee6bd2"
-PKG_SHA256="de22be374595466f81ba8f2d7a94bcd2fa6ce77b6673a4f580c78b4ec024eab3"
+PKG_VERSION="3561a12a4cebf2bb353cc986dd2ded2a97b34f30"
+PKG_SHA256="6fabd9f016ad283c1b50eca05312975163757315d5e8b62868e41896e0fefd9c"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/retroarch-assets"
 PKG_URL="https://github.com/libretro/retroarch-assets/archive/$PKG_VERSION.tar.gz"
