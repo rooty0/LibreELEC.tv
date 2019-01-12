a158737b23e4ddcf87e48ff6de6da917a7e43758
5schatten <supervisedthinking@gmail.com>
2019-01-13 00:03:07 +0100

    pcre2: updated to 10.32 / updated configure opts

--- packages/5schatten/depends/pcre2/package.mk
+++ packages/5schatten/depends/pcre2/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="pcre2"
-PKG_VERSION="10.31"
-PKG_SHA256="e07d538704aa65e477b6a392b32ff9fc5edf75ab9a40ddfc876186c4ff4d68ac"
+PKG_VERSION="10.32"
+PKG_SHA256="f29e89cc5de813f45786580101aaee3984a65818631d4ddbda7b32f699b87c2e"
 PKG_LICENSE="BSD"
 PKG_SITE="http://www.pcre.org/"
 PKG_URL="https://ftp.pcre.org/pub/pcre/$PKG_NAME-$PKG_VERSION.tar.bz2"
@@ -13,16 +13,14 @@ PKG_TOOLCHAIN="configure"
 PKG_BUILD_FLAGS="+pic"
 
 PKG_CONFIGURE_OPTS_HOST="--prefix=$TOOLCHAIN \
-             --enable-utf8 \
-             --enable-unicode-properties \
-             --with-gnu-ld"
+                         --enable-jit \
+                         --with-gnu-ld"
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-shared \
-             --enable-static \
-             --enable-utf8 \
-             --enable-pcre2-16 \
-             --enable-unicode-properties \
-             --with-gnu-ld"
+                           --enable-static \
+                           --enable-pcre2-16 \
+                           --enable-jit \
+                           --with-gnu-ld"
 
 post_makeinstall_target() {
   rm -rf $INSTALL/usr/bin
