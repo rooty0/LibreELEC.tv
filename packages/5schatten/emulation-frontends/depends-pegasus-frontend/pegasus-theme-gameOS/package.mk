11bb2ff81c2e255f72697deb5cb358de3b37dfc9
5schatten <supervisedthinking@gmail.com>
2019-01-20 19:56:52 +0100

    pegasus-theme-gameOS: updated to git 8d94787

--- packages/5schatten/emulation-frontends/depends-pegasus-frontend/pegasus-theme-gameOS/package.mk
+++ packages/5schatten/emulation-frontends/depends-pegasus-frontend/pegasus-theme-gameOS/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="pegasus-theme-gameOS"
-PKG_VERSION="e08c23c540a0ff19e8d40cfd8fa3f5ae1af1b4f3"
-PKG_SHA256="3b69134e430fc7dd9214b350580c33c736b1766d57ac8270e419661347f75300"
+PKG_VERSION="8d947875c9b805a5c7f66b4c502ef34224bf2b5f" # v0.5
+PKG_SHA256="5048dbf4b37a9d790a8a8f0f1151e115e1d453d87975594754690ce821c20d70"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/PlayingKarrde/gameOS"
 PKG_URL="https://github.com/PlayingKarrde/gameOS/archive/$PKG_VERSION.tar.gz"
