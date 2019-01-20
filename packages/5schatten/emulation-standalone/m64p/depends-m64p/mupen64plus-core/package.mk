4de9a5570705be66db1e94b6690daa59ac523df7
5schatten <supervisedthinking@gmail.com>
2019-01-20 20:00:19 +0100

    mupen64plus-core: updated to git 1b48112

--- packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-core/package.mk
+++ packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-core/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="mupen64plus-core"
-PKG_VERSION="ef15526ecdcde8f984a6162f9b874aa87108a3d4"
-PKG_SHA256="e2403e8915032e519dc6b94f46820b10e05830992d46806ca47172020c8246cc"
+PKG_VERSION="1b481123c2ac594b91fd7bb0bc1b0de0707c7b14"
+PKG_SHA256="aec474dd3a827a0c1677a4d6252fc730758732787dfbe1a230dc7142b20fb54c"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/mupen64plus/mupen64plus-core"
 PKG_URL="https://github.com/mupen64plus/mupen64plus-core/archive/$PKG_VERSION.tar.gz"
@@ -16,7 +16,7 @@ if [ "${PROJECT}" = "Generic" ]; then
   PKG_DEPENDS_TARGET+=" nasm:host glu"
 fi
 
-PKG_MAKE_OPTS_TARGET="-C projects/unix SRCDIR=../../src all SHAREDIR=/usr/config/mupen64plus"
+PKG_MAKE_OPTS_TARGET="-C projects/unix SRCDIR=../../src all NEW_DYNAREC=1 SHAREDIR=/usr/config/mupen64plus"
 
 pre_configure_target() {
   export SYSROOT_PREFIX=$SYSROOT_PREFIX
