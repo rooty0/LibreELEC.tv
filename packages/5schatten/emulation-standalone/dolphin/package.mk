201ef6ab46aa5f2803c2370e419f8022fddec5bd
5schatten <supervisedthinking@gmail.com>
2019-02-08 18:34:25 +0100

    dolphin: dropped gtk2(gtk+) dependency

--- packages/5schatten/emulation-standalone/dolphin/package.mk
+++ packages/5schatten/emulation-standalone/dolphin/package.mk
@@ -10,7 +10,7 @@ PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/dolphin-emu/dolphin"
 PKG_URL="https://github.com/dolphin-emu/dolphin/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain openal-soft libevdev gtk+ ffmpeg zlib bluez pulseaudio alsa-lib libogg libvorbis libSM enet qt-everywhere"
+PKG_DEPENDS_TARGET="toolchain openal-soft libevdev ffmpeg zlib bluez pulseaudio alsa-lib libogg libvorbis libSM enet qt-everywhere"
 PKG_LONGDESC="Dolphin is a GameCube / Wii emulator, allowing you to play games for these two platforms on PC with improvements."
 
 PKG_CMAKE_OPTS_TARGET="-DENABLE_LTO=off \
