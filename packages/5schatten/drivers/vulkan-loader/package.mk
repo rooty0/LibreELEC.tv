fd9aa24e252b8d9eb938a59a21753ef581a4b814
5schatten <supervisedthinking@gmail.com>
2019-02-09 15:39:29 +0100

    vulkan-loader: fixed dependencies

--- packages/5schatten/drivers/vulkan-loader/package.mk
+++ packages/5schatten/drivers/vulkan-loader/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="f45b8e049a614ed22b0a7f2de4d60eac22f55db26416537c33e496a528d70327"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain cmake:host vulkan-headers"
+PKG_DEPENDS_TARGET="toolchain cmake:host vulkan-headers libXrandr"
 PKG_LONGDESC="Vulkan Installable Client Driver (ICD) Loader."
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_TESTS=Off \
