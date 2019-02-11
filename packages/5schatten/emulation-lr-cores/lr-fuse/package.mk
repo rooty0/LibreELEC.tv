45e642ecb535e55a1c1e8abaad85b3fbc6afa0cc
5schatten <supervisedthinking@gmail.com>
2019-02-12 00:40:09 +0100

    lr-fuse: updated dependencies & package config for multithreaded build

--- packages/5schatten/emulation-lr-cores/lr-fuse/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-fuse/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="43552fc1ca6abee93d53bb616519815a4494339aa1dbc989c732523a8faebcf7"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/fuse-libretro"
 PKG_URL="https://github.com/libretro/fuse-libretro/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain retroarch"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="fuse-libretro is an work in progress port of the Fuse Unix Spectrum Emulator to libretro."
 
 PKG_LIBNAME="fuse_libretro.so"
