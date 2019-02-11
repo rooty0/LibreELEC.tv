39268f728cc03e842176e2afad5b99c1e8c0967b
5schatten <supervisedthinking@gmail.com>
2019-02-12 00:40:18 +0100

    lr-gambatte: updated dependencies & package config for multithreaded build

--- packages/5schatten/emulation-lr-cores/lr-gambatte/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-gambatte/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="e7009ed51e9ca5173349df9656cd90796b66806a2313eda9b27dd8eb6bbdac92"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/gambatte-libretro"
 PKG_URL="https://github.com/libretro/gambatte-libretro/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain retroarch"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Gambatte is an accuracy-focused, open-source, cross-platform Game Boy Color emulator written in C++."
 
 PKG_LIBNAME="gambatte_libretro.so"
