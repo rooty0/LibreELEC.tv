1f9cf3b4931db7bf0b51665209ff201516ae9e9b
5schatten <supervisedthinking@gmail.com>
2019-02-12 00:39:02 +0100

    lr-bsnes-mercury-accuracy: updated dependencies & package config for multithreaded build

--- packages/5schatten/emulation-lr-cores/lr-bsnes-mercury-accuracy/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-bsnes-mercury-accuracy/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="ad493381c5736e521996b850918e0df7c09124c71d98fac6a1e04c57830dade4"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/bsnes-mercury"
 PKG_URL="https://github.com/libretro/bsnes-mercury/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain retroarch"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Fork of bsnes with various performance improvements."
 PKG_TOOLCHAIN="make"
 
