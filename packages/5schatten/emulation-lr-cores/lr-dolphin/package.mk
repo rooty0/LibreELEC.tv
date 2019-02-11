00d74106a0b7c2599e90f1e7bb32704b8fb22533
5schatten <supervisedthinking@gmail.com>
2019-02-12 00:39:33 +0100

    lr-dolphin: updated dependencies & package config for multithreaded build

--- packages/5schatten/emulation-lr-cores/lr-dolphin/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-dolphin/package.mk
@@ -8,16 +8,18 @@ PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/dolphin"
 PKG_URL="https://github.com/libretro/dolphin/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain retroarch enet bluez lzo alsa-lib ffmpeg curl libpng zlib"
+PKG_DEPENDS_TARGET="toolchain linux glibc systemd mesa xorg-server enet bluez lzo alsa-lib ffmpeg curl libpng zlib"
 PKG_LONGDESC="Dolphin is a GameCube / Wii emulator, allowing you to play games for these two platforms on PC with improvements."
 
 PKG_LIBNAME="dolphin_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
 
-PKG_CMAKE_OPTS_TARGET="-DENABLE_QT=OFF \
-                       -DENABLE_LTO=OFF \
-                       -DLIBRETRO=ON \
-                       -DENABLE_ANALYTICS=OFF"
+pre_configure_target() {
+  PKG_CMAKE_OPTS_TARGET="-DENABLE_QT=OFF \
+                         -DENABLE_LTO=OFF \
+                         -DLIBRETRO=ON \
+                         -DENABLE_ANALYTICS=OFF"
+}
 
 pre_make_target() {
   # fix cross compiling
