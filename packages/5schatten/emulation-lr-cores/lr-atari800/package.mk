431043328e681eff60f158f1ef956673432b6092
5schatten <supervisedthinking@gmail.com>
2019-02-12 00:37:40 +0100

    lr-atari800: updated dependencies & package config for multithreaded build

--- packages/5schatten/emulation-lr-cores/lr-atari800/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-atari800/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="65ce8a34cd374c8accfca80f95de23a95b14644cbddc467ed9ea7f877175e59c"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/libretro-atari800"
 PKG_URL="https://github.com/libretro/libretro-atari800/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain retroarch"
+PKG_DEPENDS_TARGET="toolchain linux glibc zlib"
 PKG_LONGDESC="WIP Libretro port of Atari800 emulator version 3.1.0"
 PKG_TOOLCHAIN="make"
 
