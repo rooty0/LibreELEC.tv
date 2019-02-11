a4314b47b9034fb440e3ec81d0b03cce0600bbfd
5schatten <supervisedthinking@gmail.com>
2019-02-12 00:38:08 +0100

    lr-beetle-psx: updated dependencies & package config for multithreaded build

--- packages/5schatten/emulation-lr-cores/lr-beetle-psx/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-beetle-psx/package.mk
@@ -7,11 +7,23 @@ PKG_SHA256="5c9ae8e49622b95df6485dffe75f90e9282542a3b6d7b5331a8e48287b355302"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-psx-libretro"
 PKG_URL="https://github.com/libretro/beetle-psx-libretro/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain retroarch"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Standalone port/fork of Mednafen PSX to the Libretro API."
 
 PKG_LIBNAME="mednafen_psx_libretro.so"
 PKG_LIBPATH="${PKG_LIBNAME}"
+
+configure_package() {
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" xorg-server"
+  fi
+
+  # OpenGL Support
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  fi
+}
 
 make_target() {
   make GIT_VERSION=${PKG_VERSION:0:7}
