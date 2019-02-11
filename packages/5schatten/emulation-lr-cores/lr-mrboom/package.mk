4a3385da06e3be1761df6ba6bd0d391c9d645bd0
5schatten <supervisedthinking@gmail.com>
2019-02-12 00:41:26 +0100

    lr-mrboom: updated dependencies & package config for multithreaded build

--- packages/5schatten/emulation-lr-cores/lr-mrboom/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mrboom/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="5cce743b2cd4f7252c2d3491b326612b2a9174dcf31f8a1e5ec8dfbcff9551e1"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/mrboom-libretro"
 PKG_URL="https://github.com/libretro/mrboom-libretro/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain retroarch"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Mr.Boom is an 8 player Bomberman clone for RetroArch/Libretro"
 PKG_TOOLCHAIN="make"
 
