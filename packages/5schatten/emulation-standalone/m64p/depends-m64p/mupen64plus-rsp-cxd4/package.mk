2850abcbf75e4c6c4d79969fd3f5d6e18c2f58ac
5schatten <supervisedthinking@gmail.com>
2019-02-12 00:45:22 +0100

    mupen64plus-rsp-cxd4: updated dependencies & package config for multithreaded build

--- packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-rsp-cxd4/package.mk
+++ packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-rsp-cxd4/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="49de602b33428fffce20af7d1320d370c3a529251961239a9768e8bdc7489fd6"
 PKG_LICENSE="CC0-1.0"
 PKG_SITE="https://github.com/mupen64plus/mupen64plus-rsp-cxd4"
 PKG_URL="https://github.com/mupen64plus/mupen64plus-rsp-cxd4/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain SDL2-git mupen64plus-core"
+PKG_DEPENDS_TARGET="toolchain linux glibc SDL2-git mupen64plus-core"
 PKG_LONGDESC="Exemplary MSP communications simulator using a normalized VU."
 PKG_TOOLCHAIN="make"
 
