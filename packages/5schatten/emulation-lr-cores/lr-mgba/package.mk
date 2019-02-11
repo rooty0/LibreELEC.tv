118aa9948e02c1def07b1bd72fc745aa0ac8c78d
5schatten <supervisedthinking@gmail.com>
2019-02-11 16:39:47 +0100

    lr-mgba: reworked package configuration

--- packages/5schatten/emulation-lr-cores/lr-mgba/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mgba/package.mk
@@ -13,23 +13,29 @@ PKG_LONGDESC="mGBA is an emulator for running Game Boy Advance games."
 PKG_LIBNAME="mgba_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
 
-PKG_CMAKE_OPTS_TARGET="-DUSE_DEBUGGERS=OFF \
-                       -DBUILD_QT=OFF \
-                       -DBUILD_SDL=OFF \
-                       -DBUILD_LIBRETRO=ON \
-                       -DSKIP_LIBRARY=ON \
-                       -DUSE_FFMPEG=OFF \
-                       -DUSE_ZLIB=ON \
-                       -DUSE_MINIZIP=OFF \
-                       -DUSE_LIBZIP=OFF \
-                       -DUSE_MAGICK=OFF \
-                       -DUSE_ELF=OFF"
+configure_package() {
+  if [ "${OPENGL_SUPPORT}" = "yes" -a "${PROJECT}" = "Generic" ]; then
+    PKG_DEPENDS_TARGET+=" libepoxy"
+  fi
+}
 
-if [ "${OPENGL_SUPPORT}" = "yes" -a "${PROJECT}" = "Generic" ]; then
-  PKG_DEPENDS_TARGET+=" libepoxy"
-elif [ "${OPENGLES_SUPPORT}" = "yes" ]; then
-  PKG_CMAKE_OPTS_TARGET+=" -DBUILD_GLES2=ON"
-fi
+pre_configure_target() {
+  PKG_CMAKE_OPTS_TARGET="-DUSE_DEBUGGERS=OFF \
+                         -DBUILD_QT=OFF \
+                         -DBUILD_SDL=OFF \
+                         -DBUILD_LIBRETRO=ON \
+                         -DSKIP_LIBRARY=ON \
+                         -DUSE_FFMPEG=OFF \
+                         -DUSE_ZLIB=ON \
+                         -DUSE_MINIZIP=OFF \
+                         -DUSE_LIBZIP=OFF \
+                         -DUSE_MAGICK=OFF \
+                         -DUSE_ELF=OFF"
+
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_CMAKE_OPTS_TARGET+=" -DBUILD_GLES2=ON"
+  fi
+}
 
 pre_make_target() {
   sed -e "s/set(VERSION_STRING \${GIT_BRANCH}-\${GIT_REV}-\${GIT_COMMIT_SHORT})/set(VERSION_STRING master-${PKG_VERSION:0:7})/" -i ${PKG_BUILD}/version.cmake
