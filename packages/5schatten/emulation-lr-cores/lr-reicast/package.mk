1c767534704daa3279f6b98e6312f1cdc37f8664
5schatten <supervisedthinking@gmail.com>
2019-02-12 00:42:33 +0100

    lr-reicast: updated dependencies & package config for multithreaded build

--- packages/5schatten/emulation-lr-cores/lr-reicast/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-reicast/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="e221491dac9c1c7b225b8f8565b6daa9d97d98842fe1fbdead06b46cd3f605f4"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/reicast-emulator"
 PKG_URL="https://github.com/libretro/reicast-emulator/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain retroarch"
+PKG_DEPENDS_TARGET="toolchain linux glibc"
 PKG_LONGDESC="Reicast is a multiplatform Sega Dreamcast emulator"
 PKG_TOOLCHAIN="make"
 PKG_BUILD_FLAGS="-gold"
@@ -16,6 +16,23 @@ PKG_LIBNAME="reicast_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
 
 PKG_MAKE_OPTS_TARGET="HAVE_OPENMP=0 GIT_VERSION=${PKG_VERSION:0:7} WITH_DYNAREC=$ARCH"
+
+configure_package() {
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" xorg-server"
+  fi
+
+  # OpenGL Support
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  fi
+
+  # OpenGLES Support
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  fi
+}
 
 pre_make_target() {
   export BUILD_SYSROOT=$SYSROOT_PREFIX
