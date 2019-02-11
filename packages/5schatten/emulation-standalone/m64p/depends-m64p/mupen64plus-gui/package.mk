ddfc2cf85da7952cd18a4579bfe15dc3cce1022c
5schatten <supervisedthinking@gmail.com>
2019-02-12 00:44:58 +0100

    mupen64plus-gui: updated dependencies & package config for multithreaded build

--- packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-gui/package.mk
+++ packages/5schatten/emulation-standalone/m64p/depends-m64p/mupen64plus-gui/package.mk
@@ -7,9 +7,26 @@ PKG_SHA256="2d57f2549d4d0a827059f5bbb4be6403c7596056067b072e883bfeaf3ef90ba4"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/m64p/mupen64plus-gui"
 PKG_URL="https://github.com/m64p/mupen64plus-gui/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain SDL2-git qt-everywhere p7zip-system"
+PKG_DEPENDS_TARGET="toolchain linux glibc SDL2-git qt-everywhere p7zip-system libpng zlib"
 PKG_LONGDESC="mupen64plus GUI written in Qt5"
 PKG_TOOLCHAIN="manual"
+
+configure_package() {
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" xorg-server"
+  fi
+
+  # OpenGL Support
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  fi
+
+  # OpenGLES Support
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  fi
+}
 
 configure_target() {
   export SYSROOT_PREFIX=$SYSROOT_PREFIX
