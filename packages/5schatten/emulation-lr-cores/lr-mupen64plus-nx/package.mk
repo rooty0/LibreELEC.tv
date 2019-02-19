f403854a757a20bdf685f04d65c8fb3fff0601d0
5schatten <supervisedthinking@gmail.com>
2019-02-19 17:29:50 +0100

    lr-mupen64plus-nx: initial package git 1c2029a

---
+++ packages/5schatten/emulation-lr-cores/lr-mupen64plus-nx/package.mk
@@ -0,0 +1,74 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
+
+PKG_NAME="lr-mupen64plus-nx"
+PKG_VERSION="1c2029abcf15b486a5e7ae5517521c1559f31d37"
+PKG_SHA256="ac93c9c9ba56e037f14b8fbd191b110bf41709ccee446e07589b35d3ced5971b"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/libretro/mupen64plus-libretro-nx"
+PKG_URL="https://github.com/libretro/mupen64plus-libretro-nx/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain linux glibc zlib libpng"
+PKG_LONGDESC="Mupen64Plus is mupen64plus + GLideN64 + libretro"
+PKG_TOOLCHAIN="make"
+
+PKG_LIBNAME="mupen64plus_next_libretro.so"
+PKG_LIBPATH="$PKG_LIBNAME"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
+
+configure_package() {
+  # Displayserver Support
+  if [ "${DISPLAYSERVER}" = "x11" ]; then
+    PKG_DEPENDS_TARGET+=" xorg-server"
+  fi
+
+  # OpenGL Support
+  if [ "${OPENGL_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGL}"
+  fi
+
+  # OpenGLES Support
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+  fi
+}
+
+pre_configure_package() {
+  # OpenGLES Support
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_MAKE_OPTS_TARGET+=" FORCE_GLES=1"
+  fi
+
+  if [ "$PROJECT" = "RPi" ]; then
+    case $DEVICE in
+      RPi)
+        PKG_MAKE_OPTS_TARGET+=" platform=rpi"
+        CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads \
+                        -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux"
+
+        ;;
+      RPi2)
+        PKG_MAKE_OPTS_TARGET+=" platform=rp2"
+        CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads \
+                        -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux"
+
+        ;;
+    esac
+  else
+    # Dynarec
+    if [ "${ARCH}" = "arm" ]; then
+      PKG_MAKE_OPTS_TARGET+=" WITH_DYNAREC=arm"
+    elif [ "${ARCH}" = "x86_64" ]; then
+      PKG_MAKE_OPTS_TARGET+=" WITH_DYNAREC=x86_64"
+    fi
+    # NEON Support
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+=" HAVE_NEON=1"
+    fi
+  fi
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp $PKG_LIBPATH $INSTALL/usr/lib/libretro/
+}
