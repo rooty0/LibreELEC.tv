f73f3a9a27a1d1ea3cbf23cbf235a6ba9d751002
5schatten <supervisedthinking@gmail.com>
2019-02-19 23:16:20 +0100

    lr-beetle-saturn: updated to git 052f6d6

--- packages/5schatten/emulation-lr-cores/lr-beetle-saturn/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-beetle-saturn/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-beetle-saturn"
-PKG_VERSION="ad74e9675ad60211ae5d7bb6bbdfbae234110a55"
-PKG_SHA256="4820b641bd85fb51edfacd01f3518f03dbaa91f71a9900a9760e73ce67bf9064"
+PKG_VERSION="052f6d64ee3ea1ef5352f0b03e152743c1025eb5"
+PKG_SHA256="1d0d060cd3fc69af55926604abf1576bcf8455fbf640d1af1626e7da3e186402"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/beetle-saturn-libretro"
 PKG_URL="https://github.com/libretro/beetle-saturn-libretro/archive/$PKG_VERSION.tar.gz"
