0d273e721969ba2db7fe3331f4d8b894d8659434
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-02-21 03:09:19 +0000

    rr-config-tool: added project specific settings

--- packages/5schatten/service/rr-config-tool/package.mk
+++ packages/5schatten/service/rr-config-tool/package.mk
@@ -8,19 +8,25 @@ PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="service"
-PKG_SHORTDESC="RR Config Tool"
-PKG_LONGDESC="RR Config Tool provides a graphical interface to configure audio, video etc. for the RR build."
+PKG_SHORTDESC="RR Configuration"
+PKG_LONGDESC="This addon provides a graphical interface to configure audio, video etc. for the RR build."
 PKG_TOOLCHAIN="manual"
 
 PKG_IS_ADDON="yes"
-PKG_ADDON_NAME="RR Config Tool"
+PKG_ADDON_NAME="RR Configuration"
 PKG_ADDON_TYPE="xbmc.service.library"
 PKG_MAINTAINER="5schatten"
 
 makeinstall_target() {
+  # Create directories
   mkdir -p $INSTALL/usr/share/$MEDIACENTER/addons/$PKG_SECTION.$PKG_NAME
   mkdir -p $INSTALL/usr/share/$MEDIACENTER/addons/$PKG_SECTION.$PKG_NAME/resources
-  mkdir -p $INSTALL/usr/lib/$MEDIACENTER/addons/$PKG_SECTION.$PKG_NAME
+ 
+  # Install files
   cp $PKG_DIR/icons/* $INSTALL/usr/share/$MEDIACENTER/addons/$PKG_SECTION.$PKG_NAME/resources
   install_addon_files $INSTALL/usr/share/$MEDIACENTER/addons/$PKG_SECTION.$PKG_NAME
+
+  if [ ! "${PROJECT}" = "Generic" ]; then
+    cp -rf $PKG_DIR/files/${PROJECT}/* $INSTALL/usr/share/$MEDIACENTER/addons/$PKG_SECTION.$PKG_NAME
+  fi
 }
