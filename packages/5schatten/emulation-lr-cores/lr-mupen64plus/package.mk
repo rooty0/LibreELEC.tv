6a34180afdb5e74f56545ef8ba65ff50cefc7fd4
5schatten <supervisedthinking@gmail.com>
2019-02-21 14:03:49 +0100

    lr-mupen64plus: updated pkg toolchain to make

--- packages/5schatten/emulation-lr-cores/lr-mupen64plus/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-mupen64plus/package.mk
@@ -8,11 +8,13 @@ PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mupen64plus-libretro"
 PKG_URL="https://github.com/libretro/mupen64plus-libretro/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain linux glibc zlib"
-PKG_LONGDESC="Mupen64Plus uses GLideN64 (a graphics plugin that is not available in Parallel-N64). The emulator code itself is identical to standalone mupen64plus."
-PKG_TOOLCHAIN="manual"
+PKG_LONGDESC="Mupen64Plus is mupen64plus + GLideN64 + libretro"
+PKG_TOOLCHAIN="make"
 
 PKG_LIBNAME="mupen64plus_libretro.so"
 PKG_LIBPATH="$PKG_LIBNAME"
+
+PKG_MAKE_OPTS_TARGET="GIT_VERSION=${PKG_VERSION:0:7}"
 
 configure_package() {
   # Displayserver Support
@@ -31,35 +33,38 @@ configure_package() {
   fi
 }
 
-pre_build_target() {
-  export GIT_VERSION=${PKG_VERSION:0:7}
-}
-
-make_target() {
-
-  if target_has_feature neon; then
-    export HAVE_NEON=1
+pre_configure_target() {
+  # OpenGLES Support
+  if [ "${OPENGLES_SUPPORT}" = "yes" ]; then
+    PKG_MAKE_OPTS_TARGET+=" FORCE_GLES=1"
   fi
 
   if [ "$PROJECT" = "RPi" ]; then
     case $DEVICE in
       RPi)
+        PKG_MAKE_OPTS_TARGET+=" platform=rpi"
         CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads \
                         -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux"
 
-        make FORCE_GLES=1 platform=rpi
         ;;
       RPi2)
+        PKG_MAKE_OPTS_TARGET+=" platform=rpi2"
         CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/interface/vcos/pthreads \
                         -I$SYSROOT_PREFIX/usr/include/interface/vmcs_host/linux"
 
-        make FORCE_GLES=1 platform=rpi2
         ;;
     esac
-  elif [[ "$TARGET_CPU" = "cortex-a9" ]] || [[ "$TARGET_CPU" = *"cortex-a53" ]] || [[ "$TARGET_CPU" = "cortex-a17" ]]; then
-    make FORCE_GLES=1 WITH_DYNAREC=arm
   else
-    make WITH_DYNAREC=x86_64
+    # Dynarec
+    if [ "${ARCH}" = "arm" ]; then
+      PKG_MAKE_OPTS_TARGET+=" WITH_DYNAREC=arm"
+    elif [ "${ARCH}" = "x86_64" ]; then
+      PKG_MAKE_OPTS_TARGET+=" WITH_DYNAREC=x86_64"
+    fi
+    # NEON Support
+    if target_has_feature neon; then
+      PKG_MAKE_OPTS_TARGET+=" HAVE_NEON=1"
+    fi
   fi
 }
 
