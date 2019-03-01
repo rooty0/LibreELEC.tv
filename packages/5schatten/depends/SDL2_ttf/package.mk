2b5e29895bf421d14fe70790b13045f1a4567839
5schatten <supervisedthinking@gmail.com>
2019-03-01 10:42:38 +0100

    SDL2_ttf: updated to 2.0.15 / updated patches

--- packages/5schatten/depends/SDL2_ttf/package.mk
+++ packages/5schatten/depends/SDL2_ttf/package.mk
@@ -3,16 +3,17 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="SDL2_ttf"
-PKG_VERSION="2.0.14"
-PKG_SHA256="34db5e20bcf64e7071fe9ae25acaa7d72bdc4f11ab3ce59acc768ab62fe39276"
+PKG_VERSION="2.0.15"
+PKG_SHA256="a9eceb1ad88c1f1545cd7bd28e7cbc0b2c14191d40238f531a15b01b1b22cd33"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.libsdl.org/"
 PKG_URL="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain SDL2-git"
+PKG_DEPENDS_TARGET="toolchain SDL2-git freetype"
 PKG_LONGDESC="This is a sample library which allows you to use TrueType fonts in your SDL applications"
+PKG_TOOLCHAIN="configure"
 
-if [ ! "$DISPLAYSERVER" = "x11" ]; then
-  X11="--without-x"
+PKG_CONFIGURE_OPTS_TARGET="--with-freetype-prefix=$SYSROOT_PREFIX/usr"
+
+if [ ! "${DISPLAYSERVER}" = "x11" ]; then
+  PKG_CONFIGURE_OPTS_TARGET+=" --without-x"
 fi
-
-PKG_CONFIGURE_OPTS_TARGET="--with-freetype-prefix=$SYSROOT_PREFIX/usr $X11"
