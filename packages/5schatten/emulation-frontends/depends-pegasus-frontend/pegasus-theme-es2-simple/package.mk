a60e583b17122050d2c211209f4bb8edc0b63b59
5schatten <supervisedthinking@gmail.com>
2019-03-03 20:05:54 +0100

    pegasus-theme-es2-simple: updated to git d44e3c5

--- packages/5schatten/emulation-frontends/depends-pegasus-frontend/pegasus-theme-es2-simple/package.mk
+++ packages/5schatten/emulation-frontends/depends-pegasus-frontend/pegasus-theme-es2-simple/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="pegasus-theme-es2-simple"
-PKG_VERSION="e0747beabc90fe44af07ab2b95556f142487b344"
-PKG_SHA256="07beec77be0030eb2d19eafbfdc87c6a17cbbdfd9b73abdf6e45da1baf63c3d7"
+PKG_VERSION="d44e3c5ba2d1cbdc706e2c25822f6d8f27e64aaf"
+PKG_SHA256="d5fd8ca6ef4f92a0b17129e8c33c3199c2b18b3035f8cf93dd05f60b57394a92"
 PKG_LICENSE="CC-BY-4.0"
 PKG_SITE="https://github.com/mmatyas/pegasus-theme-es2-simple"
 PKG_URL="https://github.com/mmatyas/pegasus-theme-es2-simple/archive/$PKG_VERSION.tar.gz"
