8b8c112aa49ad4449d5b37492a36475b15db7758
5schatten <supervisedthinking@gmail.com>
2019-03-14 15:12:06 +0100

    lr-tyrquake: updated to git 3ac717f

--- packages/5schatten/emulation-lr-cores/lr-tyrquake/package.mk
+++ packages/5schatten/emulation-lr-cores/lr-tyrquake/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2018-present 5schatten (https://github.com/5schatten)
 
 PKG_NAME="lr-tyrquake"
-PKG_VERSION="7d18c405b3713577865d2bbb1a654b1ad0c3cd62"
-PKG_SHA256="cb19180f50b767866d5f1d77de15797dda999400d5175c70cd6c104cea497339"
+PKG_VERSION="3ac717fb862c1f25cd7bdf4c45b1ae3c4114dc89"
+PKG_SHA256="b04399322c166a20ea6c9f3447cb27b69550558a0782fc1f7fbd1a9cd2bbbfe3"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/tyrquake"
 PKG_URL="https://github.com/libretro/tyrquake/archive/$PKG_VERSION.tar.gz"
