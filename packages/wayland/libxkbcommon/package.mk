bcc2ddf165e03a87a2e7d80794c59e97bdd27aa4
5schatten <supervisedthinking@gmail.com>
2019-03-23 11:47:44 +0100

    libxkbcommon: added meson opts for weston displayserver

--- packages/wayland/libxkbcommon/package.mk
+++ packages/wayland/libxkbcommon/package.mk
@@ -11,11 +11,15 @@ PKG_URL="http://xkbcommon.org/download/${PKG_NAME}-${PKG_VERSION}.tar.xz"
 PKG_DEPENDS_TARGET="toolchain xkeyboard-config"
 PKG_LONGDESC="xkbcommon is a library to handle keyboard descriptions."
 
-PKG_MESON_OPTS_TARGET="-Denable-wayland=false \
-                       -Denable-docs=false"
+PKG_MESON_OPTS_TARGET="-Denable-docs=false"
 
 if [ "${DISPLAYSERVER}" = "x11" ]; then
-  PKG_MESON_OPTS_TARGET+=" -Denable-x11=true"
+  PKG_MESON_OPTS_TARGET+=" -Denable-x11=true \
+                           -Denable-wayland=false"
+elif [ "${DISPLAYSERVER}" = "weston" ]; then
+  PKG_MESON_OPTS_TARGET+=" -Denable-x11=false \
+                           -Denable-wayland=true"
 else
-  PKG_MESON_OPTS_TARGET+=" -Denable-x11=false"
+  PKG_MESON_OPTS_TARGET+=" -Denable-x11=false \
+                           -Denable-wayland=false"
 fi
